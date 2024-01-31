e6ddbc3caf68dfb8401798f1c5b142bc3929a95a
vaeng <34183939+vaeng@users.noreply.github.com>
2024-01-31 12:00:33 +0100

    fix: add missing info and comma

--- exercises/practice/collatz-conjucture/create_test_table.sql
+++ exercises/practice/collatz-conjucture/create_test_table.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS tests (
 
 INSERT INTO tests (uuid, description, number, expected)
     VALUES
-        ('540a3d51-e7a6-47a5-92a3-4ad1838f0bfd', 'zero steps for one' 1, 0),
+        ('540a3d51-e7a6-47a5-92a3-4ad1838f0bfd', 'zero steps for one', 1, 0),
         ('3d76a0a6-ea84-444a-821a-f7857c2c1859', 'divide if even', 16, 4),
         ('754dea81-123c-429e-b8bc-db20b05a87b9', 'even and odd steps', 12, 9),
         ('ecfd0210-6f85-44f6-8280-f65534892ff6', 'large number of even and odd steps', 1000000, 152);
