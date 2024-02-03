15425ce34013d52e6bf407e57feaac24f8033c0b
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-02-03 08:31:00 +0100

    feat: add allergies exercise (#28)
    * feat: add allergies exercise
    * fix: remove output files and add them to gitignore

---
+++ exercises/practice/allergies/create_fixture.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS allergies;
+CREATE TABLE "allergies" (
+    "task" TEXT,
+    "item" TEXT,
+    "score" INT NOT NULL,
+    "result" TEXT
+);
+
+-- Note: the CSV file may contain literal tab, newline, carriage returns.
+
+.mode csv
+.import ./data.csv allergies
