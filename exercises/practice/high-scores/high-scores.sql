6c276d0b219ba376477f9d9d945164e87034fccc
Glenn Jackman <glenn.jackman@gmail.com>
2024-02-10 19:24:21 -0500

    Add commentary to stub file

---
+++ exercises/practice/high-scores/high-scores.sql
@@ -0,0 +1,5 @@
+-- Schema: 
+--   CREATE TABLE "scores" ("uuid" TEXT, "score" INT);
+--   CREATE TABLE "results" ("uuid" TEXT, "property" TEXT, "result" TEXT);
+
+-- Task: Given the data in the "scores" table, update the "result" field in the "results" table.
