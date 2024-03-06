577bd56c43e9418bc236f9dc8d172f1c9d0d49e2
vaeng <34183939+vaeng@users.noreply.github.com>
2024-03-06 22:04:13 +0100

    feat: add rest-api exercise

---
+++ exercises/practice/rest-api/rest-api_test.sql
@@ -0,0 +1,34 @@
+-- Create database:
+.read ./create_fixture.sql
+
+-- Read user student solution and save any output as markdown in user_output.md:
+.mode markdown
+.output user_output.md
+.read ./rest-api.sql
+.output
+
+-- Create a clean testing environment:
+.read ./create_test_table.sql
+
+-- Comparison of user input and the tests updates the status for each test:
+UPDATE tests
+SET status = 'pass'
+FROM (SELECT database, url, payload, result FROM 'rest-api') AS actual
+WHERE (actual.database, actual.url, actual.payload, json(actual.result)) = (tests.database, tests.url, tests.payload, json(tests.expected));
+
+-- Update message for failed tests to give helpful information:
+UPDATE tests
+SET message = 'Result for ' || tests.database || ' as ' || tests.payload || ' is ' || actual.result || ', but should be ' || tests.expected
+FROM (SELECT  database, payload, url, result FROM 'rest-api') AS actual
+WHERE (actual.database, actual.payload, actual.url) = (tests.database, tests.payload, tests.url) AND tests.status = 'fail';
+
+-- Save results to ./output.json (needed by the online test-runner)
+.mode json
+.once './output.json'
+SELECT name, status, message, output, test_code, task_id
+FROM tests;
+
+-- Display test results in readable form for the student:
+.mode table
+SELECT name, status, message
+FROM tests;
