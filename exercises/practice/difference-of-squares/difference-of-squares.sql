1d4b655413f3bfb540f2c7a18d5f1a4c514f7224
Simon Schoenbuchner <34340168+simisc@users.noreply.github.com>
2024-03-19 13:01:05 +0000

    Update difference-of-squares.sql
    Correct table name in stub file.

--- exercises/practice/difference-of-squares/difference-of-squares.sql
+++ exercises/practice/difference-of-squares/difference-of-squares.sql
@@ -1,2 +1,2 @@
--- Schema: CREATE TABLE "difference.of-squares" ("number" INT", property" TEXT, "result" INT);
--- Task: update the difference.of-squares table and set the result based on the number and property fields.
+-- Schema: CREATE TABLE "difference-of-squares" ("number" INT", property" TEXT, "result" INT);
+-- Task: update the difference-of-squares table and set the result based on the number and property fields.
