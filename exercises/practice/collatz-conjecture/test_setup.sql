37c8bd4869b75e96cf52074679e5c8a4e4eda2d6
vaeng <34183939+vaeng@users.noreply.github.com>
2024-03-19 15:06:59 +0100

    fixup! fix: change description column to name

--- exercises/practice/collatz-conjecture/test_setup.sql
+++ exercises/practice/collatz-conjecture/test_setup.sql
@@ -12,7 +12,7 @@ DROP TABLE IF EXISTS tests;
 CREATE TABLE IF NOT EXISTS tests (
     -- uuid and description are taken from the test.toml file
     uuid TEXT PRIMARY KEY,
-    description TEXT NOT NULL,
+    name TEXT NOT NULL,
     -- The following section is needed by the online test-runner
     status TEXT DEFAULT 'fail',
     message TEXT,
