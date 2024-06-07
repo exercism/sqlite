a7fa5c4afa72639c6103232c55a84706dd26ea20
Caleb Miller <caleb@mathmammoth.com>
2024-06-07 09:57:02 -0600

    fix example solution because the ci doesn't like it

--- exercises/practice/etl/.meta/example.sql
+++ exercises/practice/etl/.meta/example.sql
@@ -1,6 +1,6 @@
 UPDATE etl
 SET result = (
-    SELECT json_group_object(LOWER(value), TRIM(path, '$."') ->> '$')
+    SELECT json_group_object(LOWER(value), TRIM(path, '$."') + 0)
     FROM (
         SELECT value, path
         FROM json_tree(input)
