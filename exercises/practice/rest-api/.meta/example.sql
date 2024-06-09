cc889967c85adde2286be1b9e16c6fb9ec274a58
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-06-09 09:19:18 +0200

    feat: finish example solution and improve test

--- exercises/practice/rest-api/.meta/example.sql
+++ exercises/practice/rest-api/.meta/example.sql
@@ -1,51 +1,90 @@
 UPDATE 'rest-api' AS CURRENT
 SET result = (
-    SELECT json_object('users', IIF(json_type(DB.value) IS NULL, json_array(), json_array(DB.value)))
-    FROM 'rest-api' AS RA
-        LEFT JOIN json_each(RA.payload, '$.users') AS PL ON RA.payload = PL.json
-        LEFT JOIN json_each(RA.database, '$.users') AS DB ON RA.database = DB.json AND json_extract(DB.value, '$.name') = PL.value
-    WHERE RA.database = CURRENT.database AND RA.payload = CURRENT.payload 
+  SELECT json_object('users', IIF(json_type(DB.value) IS NULL, json_array(), json_array(DB.value)))
+  FROM 'rest-api' AS RA
+    LEFT JOIN json_each(RA.payload, '$.users') AS PL ON RA.payload = PL.json
+    LEFT JOIN json_each(RA.database, '$.users') AS DB ON RA.database = DB.json AND DB.value ->> 'name' = PL.value
+  WHERE (RA.database, RA.payload) = (CURRENT.database, CURRENT.payload)
 )
 WHERE url = '/users';
 
-UPDATE 'rest-api' AS CURRENT
-SET result = json_object('name',json_extract(payload, '$.user'), "owes", json_object(), "owed_by", json_object(), "balance", 0)
-WHERE url = '/add';
 
+UPDATE 'rest-api' AS CURRENT
+SET result = json_object('name', payload ->> 'user', 'owes', json_object(), 'owed_by', json_object(), 'balance', 0)
+WHERE url = '/add';
 
 
 UPDATE 'rest-api' AS CURRENT
 SET result = json_array(
-    -- update the lender:
-    (SELECT json_object('name', json_extract(payload, '$.lender'),
-                        'owes', json_extract(value, '$.owes'),
-                        'owed_by', IIF( json_extract(payload, '$.amount') + json_extract(value, '$.owes.'|| json_extract(payload, '$.borrower')) IS NULL,
-                                        json_patch(json_extract(value, '$.owed_by'), json_object(json_extract(payload, '$.borrower'), NULL)),
-                                        json_set(json_extract(value, '$.owed_by'), '$.'|| json_extract(payload, '$.borrower'),  json_extract(payload, '$.amount') + json_extract(value, '$.owes.'|| json_extract(payload, '$.borrower')) )),
-                        'balance', json_extract(value, '$.balance') + json_extract(payload, '$.amount'))
-    FROM json_each(database, '$.users')
-    WHERE json = database AND json_extract(value, '$.name') == json_extract(payload, '$.lender')
-    ),
-    -- update the borrower:
-    (
-    SELECT
-        json_object('name', json_extract(payload, '$.borrower'),
-                    'owes', json_set(json_extract(value, '$.owes'), '$.'|| json_extract(payload, '$.lender'),  json_extract(payload, '$.amount')),
-                    'owed_by', json_extract(value, '$.owed_by'),
-                    'balance', json_extract(value, '$.balance') - json_extract(payload, '$.amount'))
-    FROM json_each(database, '$.users')
-    WHERE json = database AND json_extract(value, '$.name') == json_extract(payload, '$.borrower')
+  -- update the lender:
+  (
+    SELECT json_object(
+      'name', payload ->> 'lender',
+      'owes', json_patch(
+        value -> 'owes',
+        json_object(
+          payload ->> 'borrower',
+          IIF(relative_balance < 0, -relative_balance, NULL)
+        )
+      ),
+      'owed_by', json_patch(
+        value -> 'owed_by',
+        json_object(
+          payload ->> 'borrower',
+          IIF(relative_balance > 0, relative_balance, NULL)
+        )
+      ),
+      'balance', (value ->> 'balance') + (payload ->> 'amount')
     )
-
+    FROM (
+      SELECT
+        *,
+        IFNULL(value -> 'owed_by' ->> (payload ->> 'borrower'), 0) -
+        IFNULL(value -> 'owes' ->> (payload ->> 'borrower'), 0) + (payload ->> 'amount') AS relative_balance
+      FROM json_each(database, '$.users')
+      WHERE json = database AND value ->> 'name' == payload ->> 'lender'
     )
+  ),
+  -- update the borrower:
+  (
+    SELECT json_object(
+      'name', payload ->> 'borrower',
+      'owes', json_patch(
+        value -> 'owes',
+        json_object(
+          payload ->> 'lender',
+          IIF(relative_balance < 0, -relative_balance, NULL)
+        )
+      ),
+      'owed_by', json_patch(
+        value -> 'owed_by',
+        json_object(
+          payload ->> 'lender',
+          IIF(relative_balance > 0, relative_balance, NULL)
+        )
+      ),
+      'balance', (value ->> 'balance') - (payload ->> 'amount')
+    )
+    FROM (
+      SELECT
+        *,
+        IFNULL(value -> 'owed_by' ->> (payload ->> 'lender'), 0) -
+        IFNULL(value -> 'owes' ->> (payload ->> 'lender'), 0) - (payload ->> 'amount') AS relative_balance
+      FROM json_each(database, '$.users')
+      WHERE json = database AND value ->> 'name' == payload ->> 'borrower'
+    )
+  )
+)
 WHERE url = '/iou';
 
 -- order the result
 UPDATE 'rest-api' AS CURRENT
 SET result = (
-    SELECT json_object('users', json_group_array(json(value)))
-    FROM (
-        SELECT value
-        FROM json_each(result)
-        ORDER BY value))
+  SELECT json_object('users', json_group_array(json(value)))
+  FROM (
+    SELECT value
+    FROM json_each(result)
+    ORDER BY value
+  )
+)
 WHERE url = '/iou';
--- exercises/practice/rest-api/rest-api_test.sql
+++ exercises/practice/rest-api/rest-api_test.sql
@@ -14,7 +14,10 @@
 UPDATE tests
 SET status = 'pass'
 FROM (SELECT database, url, payload, result FROM 'rest-api') AS actual
-WHERE (actual.database, actual.url, actual.payload, json(actual.result)) = (tests.database, tests.url, tests.payload, json(tests.expected));
+WHERE (actual.database, actual.url, actual.payload) = (tests.database, tests.url, tests.payload)
+AND (SELECT COUNT(*) FROM json_tree(actual.result) as a, json_tree(tests.expected) as b WHERE (a.fullkey, a.atom) IS (b.fullkey, b.atom)) =
+    (SELECT COUNT(*) FROM json_tree(tests.expected))
+AND (SELECT COUNT(*) FROM json_tree(tests.expected)) = (SELECT COUNT(*) FROM json_tree(actual.result));
 
 -- Update message for failed tests to give helpful information:
 UPDATE tests
