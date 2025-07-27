c58bceac14295c127faa3f87347aa677a70f71f4
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-07-26 23:32:24 -0300

    * replacing function CONCAT_WS with CONCAT

--- exercises/practice/resistor-color-trio/resistor-color-trio_test.sql
+++ exercises/practice/resistor-color-trio/resistor-color-trio_test.sql
@@ -20,7 +20,7 @@ WHERE (actual.color1, actual.color2, actual.color3, actual.result) = (tests.colo
 UPDATE tests
 SET message = (
     'Result for "'
-    || CONCAT_WS(',', tests.color1, tests.color2, tests.color3)
+    || CONCAT(tests.color1, ',', tests.color2, ',', tests.color3)
     || '"'
     || ' is <' || COALESCE(actual.result, 'NULL')
     || '> but should be <' || tests.expected || '>'
