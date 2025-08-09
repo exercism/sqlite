3c6412e0c2e9d17638e2245f955b492f45675ed1
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-09 09:06:09 -0300

    * adjusting code style

--- exercises/practice/bottle-song/.meta/example.sql
+++ exercises/practice/bottle-song/.meta/example.sql
@@ -1,41 +1,37 @@
 DROP TABLE IF EXISTS pairs;
 CREATE TEMPORARY TABLE pairs (
-  n INTEGER NOT NULL,
-  a TEXT NOT NULL,
-  b TEXT NOT NULL
+    verse_number INTEGER NOT NULL,
+    start_number TEXT    NOT NULL,
+    next_number  TEXT    NOT NULL
 );
-INSERT INTO pairs (n, a, b)
-VALUES
-(10, 'Ten'  , 'Nine' ),
-( 9, 'Nine' , 'Eight'),
-( 8, 'Eight', 'Seven'),
-( 7, 'Seven', 'Six'  ),
-( 6, 'Six'  , 'Five' ),
-( 5, 'Five' , 'Four' ),
-( 4, 'Four' , 'Three'),
-( 3, 'Three', 'Two'  ),
-( 2, 'Two'  , 'One'  ),
-( 1, 'One'  , 'no'   );
+INSERT INTO pairs (verse_number, start_number, next_number)
+VALUES (10, 'Ten'  , 'Nine' ),
+       ( 9, 'Nine' , 'Eight'),
+       ( 8, 'Eight', 'Seven'),
+       ( 7, 'Seven', 'Six'  ),
+       ( 6, 'Six'  , 'Five' ),
+       ( 5, 'Five' , 'Four' ),
+       ( 4, 'Four' , 'Three'),
+       ( 3, 'Three', 'Two'  ),
+       ( 2, 'Two'  , 'One'  ),
+       ( 1, 'One'  , 'no'   );
 
 DROP TABLE IF EXISTS verses;
 CREATE TABLE verses AS
-  SELECT n,
-         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), a) ||
-         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), a) ||
+  SELECT verse_number,
+         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), start_number) ||
+         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), start_number) ||
          'And if one green bottle should accidentally fall,' || CHAR(10) ||
-         PRINTF(
-           'There''ll be %s green bottles hanging on the wall.',
-           LOWER(b)
-         ) AS verse
+         PRINTF('There''ll be %s green bottles hanging on the wall.',LOWER(next_number)) AS verse
   FROM pairs
 ;
 
 UPDATE verses
    SET verse = REPLACE(verse, 'be one green bottles', 'be one green bottle')
- WHERE n = 2;
+ WHERE verse_number = 2;
 UPDATE verses
    SET verse = REPLACE(verse, 'One green bottles', 'One green bottle')
- WHERE n = 1;
+ WHERE verse_number = 1;
 
 
 UPDATE "bottle-song"
@@ -44,9 +40,9 @@ UPDATE "bottle-song"
        FROM (
          SELECT verse
            FROM verses
-          WHERE n <= start_bottles
-            AND n > start_bottles - take_down
-          ORDER BY n DESC
+          WHERE verse_number <= start_bottles
+            AND verse_number > start_bottles - take_down
+          ORDER BY verse_number DESC
        )
    )
 ;
