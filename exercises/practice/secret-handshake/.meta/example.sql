6b8e79112a8d8abc43cceada7dd9fe1e6eaa4759
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-19 01:39:35 -0300

    * IIF() version demands three arguments

--- exercises/practice/secret-handshake/.meta/example.sql
+++ exercises/practice/secret-handshake/.meta/example.sql
@@ -4,10 +4,10 @@ UPDATE "secret-handshake"
        FROM (
          SELECT
            JSON_ARRAY(
-             IIF((number >> 3) & 1, 'jump'           ),
-             IIF((number >> 2) & 1, 'close your eyes'),
-             IIF((number >> 1) & 1, 'double blink'   ),
-             IIF((number >> 0) & 1, 'wink'           )
+             IIF((number >> 3) & 1, 'jump'           , NULL),
+             IIF((number >> 2) & 1, 'close your eyes', NULL),
+             IIF((number >> 1) & 1, 'double blink'   , NULL),
+             IIF((number >> 0) & 1, 'wink'           , NULL)
            ) AS commands
        ), JSON_EACH(commands) j
       WHERE j.value NOTNULL
@@ -21,10 +21,10 @@ UPDATE "secret-handshake"
        FROM (
          SELECT
            JSON_ARRAY(
-             IIF((number >> 0) & 1, 'wink'           ),
-             IIF((number >> 1) & 1, 'double blink'   ),
-             IIF((number >> 2) & 1, 'close your eyes'),
-             IIF((number >> 3) & 1, 'jump'           )
+             IIF((number >> 0) & 1, 'wink'           , NULL),
+             IIF((number >> 1) & 1, 'double blink'   , NULL),
+             IIF((number >> 2) & 1, 'close your eyes', NULL),
+             IIF((number >> 3) & 1, 'jump'           , NULL)
            ) AS commands
        ), JSON_EACH(commands) j
       WHERE j.value NOTNULL
