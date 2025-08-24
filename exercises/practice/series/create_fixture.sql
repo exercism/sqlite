09d5e84fcf4d7671f6a91ef47084360f492a73f3
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-24 01:08:10 -0300

    * add practice exercise: series

---
+++ exercises/practice/series/create_fixture.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS series;
+CREATE TABLE series (
+    input        TEXT    NOT NULL,
+    slice_length INTEGER NOT NULL,
+    result       TEXT            ,
+    error        TEXT
+);
+
+.mode csv
+.import ./data.csv series
+
+UPDATE series SET result = NULL, error = NULL;
