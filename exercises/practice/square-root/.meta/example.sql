11c486c50331fdd511123056001a195d2735afc2
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-24 00:27:44 -0300

    Add practice exercise: square-root (#131)

---
+++ exercises/practice/square-root/.meta/example.sql
@@ -0,0 +1,16 @@
+UPDATE "square-root"
+   SET result = (
+     WITH RECURSIVE isqrt (number, approx) AS (
+       VALUES (radicand, radicand)
+       UNION ALL
+       SELECT number, (number / approx + approx) / 2
+         FROM isqrt
+        WHERE approx * approx != NUMBER
+          AND  approx > 1
+     )
+     SELECT approx AS sqrt
+       FROM isqrt
+      ORDER BY approx ASC
+      LIMIT 1
+   )
+;
