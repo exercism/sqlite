7ecaa069f16f19282527cc620d27efebf876e25e
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-25 11:01:02 -0300

    * add practice exercise: run-length-encoding

---
+++ exercises/practice/run-length-encoding/create_fixture.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "run-length-encoding";
+CREATE TABLE "run-length-encoding" (
+    property TEXT NOT NULL,
+    string   TEXT NOT NULL,
+    result   TEXT
+);
+
+.mode csv
+.import ./data.csv "run-length-encoding"
+
+UPDATE "run-length-encoding" SET result = NULL;
