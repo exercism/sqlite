d81f66d58acc822fcdc722c39ee81adf54fc9fd6
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-26 00:35:16 -0300

    * add practice exercise: saddle-points

---
+++ exercises/practice/saddle-points/saddle-points.sql
@@ -0,0 +1,8 @@
+-- Schema:
+-- CREATE TABLE "saddle-points" (
+--     matrix TEXT NOT NULL,    -- json array of arrays
+--     result TEXT              -- json array of object
+-- );
+-- Task: update the saddle-points table and set the result based on the matrix.
+--       * the matrix column contains a JSON-encoded array of arrays of integers.
+--       * the result columns should contain JSON-encoded array of objects, each object has two keys: row and column.
