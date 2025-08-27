892e60490bbd36966db9ae806bc93b1b1e92e20e
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:20 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - nucleotide-count

--- exercises/practice/nucleotide-count/nucleotide-count.sql
+++ exercises/practice/nucleotide-count/nucleotide-count.sql
@@ -1,14 +1,10 @@
 -- Task:  - Update the "nucleotide-count" table and set the result based on the input field.
 --        - Update table creation with constraints.
-
 DROP TABLE IF EXISTS "nucleotide-count";
-CREATE TABLE "nucleotide-count" (
-    "strand" TEXT,
-    "result" TEXT
-);
+
+CREATE TABLE "nucleotide-count" ("strand" TEXT, "result" TEXT);
 
 -- Please don't change the following two import lines. Feel free to edit the previous lines, though.
 .mode csv
 .import ./data.csv "nucleotide-count"
-
 -- Write your code below. Feel free to edit the CREATE TABLE above, too!
--- exercises/practice/nucleotide-count/test_reporter.sql
+++ exercises/practice/nucleotide-count/test_reporter.sql
@@ -1,41 +1,67 @@
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for "' || tests.input || '"'
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT strand, result  FROM "nucleotide-count") AS actual
-WHERE actual.strand = tests.input AND tests.status = 'fail';
+SET
+  message = (
+    'Result for "' || tests.input || '"' || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      strand,
+      result
+    FROM
+      "nucleotide-count"
+  ) AS actual
+WHERE
+  actual.strand = tests.input
+  AND tests.status = 'fail';
 
 -- Process test cases that should fail:
 -- All error tests should pass, updated to fail if invalid entry is possible
 UPDATE tests
-SET status = 'pass'
-WHERE expected = 'error';
+SET
+  status = 'pass'
+WHERE
+  expected = 'error';
 
 -- Only triggered if insert is successful
-CREATE TRIGGER IF NOT EXISTS error_checker 
-    AFTER INSERT
-    ON "nucleotide-count"
-BEGIN
-    UPDATE tests
-    SET status = 'fail',
-        message = NEW.strand || ' should be restricted from insertion'
-    WHERE tests.input = NEW.strand;
+CREATE TRIGGER IF NOT EXISTS error_checker AFTER INSERT ON "nucleotide-count" BEGIN
+UPDATE tests
+SET
+  status = 'fail',
+  message = NEW.strand || ' should be restricted from insertion'
+WHERE
+  tests.input = NEW.strand;
+
 END;
 
-INSERT OR IGNORE INTO "nucleotide-count" (strand)
-SELECT input FROM tests
-WHERE expected = 'error';
+INSERT OR IGNORE INTO
+  "nucleotide-count" (strand)
+SELECT
+  input
+FROM
+  tests
+WHERE
+  expected = 'error';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT name, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  name,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT name, status, message
-FROM tests;
+SELECT
+  name,
+  status,
+  message
+FROM
+  tests;
