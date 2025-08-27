3a2a9b970dda51863e099bf056773abb3b4f3522
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:50 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - triangle

--- exercises/practice/triangle/.meta/example.sql
+++ exercises/practice/triangle/.meta/example.sql
@@ -1,25 +1,40 @@
 UPDATE triangle
-   SET result = FALSE
- WHERE 0 IN (side_a, side_b, side_c)
-    OR side_a + side_b < side_c
-    OR side_b + side_c < side_a
-    OR side_a + side_c < side_b
-;
+SET
+  result = FALSE
+WHERE
+  0 IN (side_a, side_b, side_c)
+  OR side_a + side_b < side_c
+  OR side_b + side_c < side_a
+  OR side_a + side_c < side_b;
 
 UPDATE triangle
-   SET result = (side_a = side_b AND side_a = side_c)
- WHERE property = 'equilateral'
-   AND result ISNULL
-;
+SET
+  result = (
+    side_a = side_b
+    AND side_a = side_c
+  )
+WHERE
+  property = 'equilateral'
+  AND result ISNULL;
 
 UPDATE triangle
-   SET result = (side_a = side_b OR side_a = side_c OR side_b = side_c)
- WHERE property = 'isosceles'
-   AND result ISNULL
-;
+SET
+  result = (
+    side_a = side_b
+    OR side_a = side_c
+    OR side_b = side_c
+  )
+WHERE
+  property = 'isosceles'
+  AND result ISNULL;
 
 UPDATE triangle
-   SET result = (side_a != side_b AND side_a != side_c AND side_b != side_c)
- WHERE property = 'scalene'
-   AND result ISNULL
-;
+SET
+  result = (
+    side_a != side_b
+    AND side_a != side_c
+    AND side_b != side_c
+  )
+WHERE
+  property = 'scalene'
+  AND result ISNULL;
