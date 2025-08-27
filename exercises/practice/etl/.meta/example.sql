7904e34e60e4f78d2e1537e2e162d7b288b9af64
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:01 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - etl

--- exercises/practice/etl/.meta/example.sql
+++ exercises/practice/etl/.meta/example.sql
@@ -1,10 +1,18 @@
 UPDATE etl
-SET result = (
-    SELECT json_group_object(LOWER(value), TRIM(path, '$."') + 0)
-    FROM (
-        SELECT value, path
-        FROM json_tree(input)
-        WHERE type = 'text'
-        ORDER BY value
-    )
-);
+SET
+  result = (
+    SELECT
+      json_group_object(LOWER(value), TRIM(path, '$."') + 0)
+    FROM
+      (
+        SELECT
+          value,
+          path
+        FROM
+          json_tree(input)
+        WHERE
+          type = 'text'
+        ORDER BY
+          value
+      )
+  );
--- exercises/practice/etl/create_fixture.sql
+++ exercises/practice/etl/create_fixture.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS "etl";
-CREATE TABLE "etl" (
-    "input" TEXT,
-    "result" TEXT
-);
+
+CREATE TABLE "etl" ("input" TEXT, "result" TEXT);
 
 .mode csv
 .import ./data.csv etl
