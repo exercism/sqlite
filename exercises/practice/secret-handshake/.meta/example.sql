1f2018814115d0b29412115a9347b41720b4a895
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:39 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - secret-handshake

--- exercises/practice/secret-handshake/.meta/example.sql
+++ exercises/practice/secret-handshake/.meta/example.sql
@@ -1,33 +1,43 @@
 UPDATE "secret-handshake"
-   SET result = (
-     SELECT COALESCE(GROUP_CONCAT(j.value, ', '), '')
-       FROM (
-         SELECT
-           JSON_ARRAY(
-             IIF((number >> 3) & 1, 'jump'           , NULL),
-             IIF((number >> 2) & 1, 'close your eyes', NULL),
-             IIF((number >> 1) & 1, 'double blink'   , NULL),
-             IIF((number >> 0) & 1, 'wink'           , NULL)
-           ) AS commands
-       ), JSON_EACH(commands) j
-      WHERE j.value NOTNULL
-   )
- WHERE (number >> 4) & 1 = 1
-;
+SET
+  result = (
+    SELECT
+      COALESCE(GROUP_CONCAT(j.value, ', '), '')
+    FROM
+      (
+        SELECT
+          JSON_ARRAY(
+            IIF((number >> 3) & 1, 'jump', NULL),
+            IIF((number >> 2) & 1, 'close your eyes', NULL),
+            IIF((number >> 1) & 1, 'double blink', NULL),
+            IIF((number >> 0) & 1, 'wink', NULL)
+          ) AS commands
+      ),
+      JSON_EACH(commands) j
+    WHERE
+      j.value NOTNULL
+  )
+WHERE
+  (number >> 4) & 1 = 1;
 
 UPDATE "secret-handshake"
-   SET result = (
-     SELECT COALESCE(GROUP_CONCAT(j.value, ', '), '')
-       FROM (
-         SELECT
-           JSON_ARRAY(
-             IIF((number >> 0) & 1, 'wink'           , NULL),
-             IIF((number >> 1) & 1, 'double blink'   , NULL),
-             IIF((number >> 2) & 1, 'close your eyes', NULL),
-             IIF((number >> 3) & 1, 'jump'           , NULL)
-           ) AS commands
-       ), JSON_EACH(commands) j
-      WHERE j.value NOTNULL
-   )
- WHERE (number >> 4) & 1 = 0
-;
+SET
+  result = (
+    SELECT
+      COALESCE(GROUP_CONCAT(j.value, ', '), '')
+    FROM
+      (
+        SELECT
+          JSON_ARRAY(
+            IIF((number >> 0) & 1, 'wink', NULL),
+            IIF((number >> 1) & 1, 'double blink', NULL),
+            IIF((number >> 2) & 1, 'close your eyes', NULL),
+            IIF((number >> 3) & 1, 'jump', NULL)
+          ) AS commands
+      ),
+      JSON_EACH(commands) j
+    WHERE
+      j.value NOTNULL
+  )
+WHERE
+  (number >> 4) & 1 = 0;
--- exercises/practice/secret-handshake/secret-handshake_test.sql
+++ exercises/practice/secret-handshake/secret-handshake_test.sql
@@ -1,40 +1,63 @@
 -- Create database:
 .read ./create_fixture.sql
-
 -- Read user student solution and save any output as markdown in user_output.md:
 .mode markdown
 .output user_output.md
 .read ./secret-handshake.sql
 .output
-
 -- Create a clean testing environment:
 .read ./create_test_table.sql
-
 -- Comparison of user input and the tests updates the status for each test:
 UPDATE tests
-SET status = 'pass'
-FROM (SELECT number, result FROM "secret-handshake") AS actual
-WHERE (actual.number, actual.result) = (tests.number, tests.expected);
+SET
+  status = 'pass'
+FROM
+  (
+    SELECT
+      number,
+      result
+    FROM
+      "secret-handshake"
+  ) AS actual
+WHERE
+  (actual.number, actual.result) = (tests.number, tests.expected);
 
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for "'
-    || tests.number
-    || '"'
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT number, result FROM "secret-handshake") AS actual
-WHERE actual.number = tests.number AND tests.status = 'fail';
+SET
+  message = (
+    'Result for "' || tests.number || '"' || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      number,
+      result
+    FROM
+      "secret-handshake"
+  ) AS actual
+WHERE
+  actual.number = tests.number
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT description, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  description,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT description, status, message
-FROM tests;
+SELECT
+  description,
+  status,
+  message
+FROM
+  tests;
