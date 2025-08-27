f59ad16b6912854a6c4145fed8139985137912c6
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:10 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - isbn-verifier

--- exercises/practice/isbn-verifier/create_fixture.sql
+++ exercises/practice/isbn-verifier/create_fixture.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS "isbn-verifier";
-CREATE TABLE "isbn-verifier" (
-    isbn   TEXT NOT NULL,
-    result BOOL
-);
+
+CREATE TABLE "isbn-verifier" (isbn TEXT NOT NULL, result BOOL);
 
 .mode csv
 .import ./data.csv "isbn-verifier"
-
-UPDATE "isbn-verifier" SET result = NULL;
+UPDATE "isbn-verifier"
+SET
+  result = NULL;
