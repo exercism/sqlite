1d5c22f202023b083f59160396046a1409295d88
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:23 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - pascals-triangle

--- exercises/practice/pascals-triangle/.meta/example.sql
+++ exercises/practice/pascals-triangle/.meta/example.sql
@@ -1,23 +1,48 @@
 UPDATE "pascals-triangle"
-SET result = (
-  WITH RECURSIVE counter(k) AS (
-    SELECT 1 UNION ALL
-    SELECT k + 1 FROM counter WHERE k < input
-  )
-
-  SELECT group_concat(
-    (
-      WITH RECURSIVE nums(idx, s) AS (
-        SELECT 1, 1 UNION ALL
-        SELECT idx + 1, s * k / idx - s FROM nums WHERE idx < k
+SET
+  result = (
+    WITH RECURSIVE
+      counter (k) AS (
+        SELECT
+          1
+        UNION ALL
+        SELECT
+          k + 1
+        FROM
+          counter
+        WHERE
+          k < input
       )
-
-      SELECT group_concat(s, ' ') FROM nums
-    ),
-    char(10)  -- newline
-  ) FROM counter
-);
+    SELECT
+      group_concat(
+        (
+          WITH RECURSIVE
+            nums (idx, s) AS (
+              SELECT
+                1,
+                1
+              UNION ALL
+              SELECT
+                idx + 1,
+                s * k / idx - s
+              FROM
+                nums
+              WHERE
+                idx < k
+            )
+          SELECT
+            group_concat(s, ' ')
+          FROM
+            nums
+        ),
+        char(10) -- newline
+      )
+    FROM
+      counter
+  );
 
 UPDATE "pascals-triangle"
-SET result = ''
-WHERE input = 0;
+SET
+  result = ''
+WHERE
+  input = 0;
