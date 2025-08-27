9005ad430588eebcba7dc81171ff780d2d4ff2ec
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:48 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - tournament

--- exercises/practice/tournament/tournament_test.sql
+++ exercises/practice/tournament/tournament_test.sql
@@ -1,40 +1,63 @@
 -- Create database:
 .read ./create_fixture.sql
-
 -- Read user student solution and save any output as markdown in user_output.md:
 .mode markdown
 .output user_output.md
 .read ./tournament.sql
 .output
-
 -- Create a clean testing environment:
 .read ./create_test_table.sql
-
 -- Comparison of user input and the tests updates the status for each test:
 UPDATE tests
-SET status = 'pass'
-FROM (SELECT input, result FROM tournament) AS actual
-WHERE (actual.input, actual.result) = (tests.input, tests.expected);
+SET
+  status = 'pass'
+FROM
+  (
+    SELECT
+      input,
+      result
+    FROM
+      tournament
+  ) AS actual
+WHERE
+  (actual.input, actual.result) = (tests.input, tests.expected);
 
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for "'
-    || tests.input
-    || '"'
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT input, result FROM tournament) AS actual
-WHERE actual.input = tests.input AND tests.status = 'fail';
+SET
+  message = (
+    'Result for "' || tests.input || '"' || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      input,
+      result
+    FROM
+      tournament
+  ) AS actual
+WHERE
+  actual.input = tests.input
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT description, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  description,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT description, status, message
-FROM tests;
+SELECT
+  description,
+  status,
+  message
+FROM
+  tests;
