f0c555fc59fd65726747edfbf4d6ca84755fe804
Isaac Good <github@isaacgood.com>
2025-08-26 22:47:50 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - armstrong-numbers

--- exercises/practice/armstrong-numbers/.meta/example.sql
+++ exercises/practice/armstrong-numbers/.meta/example.sql
@@ -5,26 +5,42 @@ ALTER TABLE "armstrong-numbers"
 ADD exp INTEGER;
 
 UPDATE "armstrong-numbers"
-SET exp = IIF(INSTR(number, 'e') != 0, SUBSTR(number,  INSTR(number, 'e') +1, LENGTH(number)), LENGTH(NUMBER));
+SET
+  exp = IIF(
+    INSTR(number, 'e') != 0,
+    SUBSTR(number, INSTR(number, 'e') + 1, LENGTH(number)),
+    LENGTH(NUMBER)
+  );
 
 UPDATE "armstrong-numbers"
-SET result = 0;
+SET
+  result = 0;
 
 PRAGMA recursive_triggers = ON;
 
-CREATE TRIGGER IF NOT EXISTS "update_armstrong-numbers"
-	AFTER UPDATE
-    ON "armstrong-numbers"
-    WHEN NEW.rest > 0
-BEGIN
-	UPDATE "armstrong-numbers"
-    SET rest = NEW.rest / 10
-        ,result = result + POWER(NEW.rest % 10, exp)
-    WHERE rest = NEW.rest;
+CREATE TRIGGER IF NOT EXISTS "update_armstrong-numbers" AFTER
+UPDATE ON "armstrong-numbers" WHEN NEW.rest > 0 BEGIN
+UPDATE "armstrong-numbers"
+SET
+  rest = NEW.rest / 10,
+  result = result + POWER(NEW.rest % 10, exp)
+WHERE
+  rest = NEW.rest;
+
 END;
 
 UPDATE "armstrong-numbers"
-SET rest = IIF(INSTR(number, 'e') != 0, SUBSTR(REPLACE(number, '.', ''), 0, INSTR(number, 'e') - 1), number);
+SET
+  rest = IIF(
+    INSTR(number, 'e') != 0,
+    SUBSTR(
+      REPLACE(number, '.', ''),
+      0,
+      INSTR(number, 'e') - 1
+    ),
+    number
+  );
 
 UPDATE "armstrong-numbers"
-SET result = IIF(result = number, TRUE, FALSE);
+SET
+  result = IIF(result = number, TRUE, FALSE);
