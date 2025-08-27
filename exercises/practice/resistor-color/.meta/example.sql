613930cc27a5431b1b3f1f73f19124732b3cca16
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:27 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - resistor-color

--- exercises/practice/resistor-color/.meta/example.sql
+++ exercises/practice/resistor-color/.meta/example.sql
@@ -1,27 +1,31 @@
 -- CREATE TABLE "color_code" ("color" TEXT, "result" INT);
-
 DROP TABLE IF EXISTS "colors";
+
 CREATE TABLE "colors" ("index" INT, "color" TEXT);
 
-INSERT INTO "colors" ("index", "color")
-    VALUES
-        (0, 'black'),
-        (1, 'brown'),
-        (2, 'red'),
-        (3, 'orange'),
-        (4, 'yellow'),
-        (5, 'green'),
-        (6, 'blue'),
-        (7, 'violet'),
-        (8, 'grey'),
-        (9, 'white')
-;
+INSERT INTO
+  "colors" ("index", "color")
+VALUES
+  (0, 'black'),
+  (1, 'brown'),
+  (2, 'red'),
+  (3, 'orange'),
+  (4, 'yellow'),
+  (5, 'green'),
+  (6, 'blue'),
+  (7, 'violet'),
+  (8, 'grey'),
+  (9, 'white');
 
 UPDATE color_code
-    SET result = (
-        SELECT "index" FROM "colors"
-        WHERE colors.color = color_code.color
-    )
-;
+SET
+  result = (
+    SELECT
+      "index"
+    FROM
+      "colors"
+    WHERE
+      colors.color = color_code.color
+  );
 
 DROP TABLE IF EXISTS "colors";
