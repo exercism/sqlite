8fea4e50df82c806d6d03efcef09eca8c8c10cbb
Isaac Good <github@isaacgood.com>
2025-08-26 22:47:58 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - difference-of-squares

--- exercises/practice/difference-of-squares/create_fixture.sql
+++ exercises/practice/difference-of-squares/create_fixture.sql
@@ -1,11 +1,7 @@
 DROP TABLE IF EXISTS "difference-of-squares";
-CREATE TABLE "difference-of-squares" (
-    "number" INT,
-    "property" TEXT,
-    "result" INT
-);
+
+CREATE TABLE "difference-of-squares" ("number" INT, "property" TEXT, "result" INT);
 
 -- Note: the CSV file may contain literal tab, newline, carriage returns.
-
 .mode csv
 .import ./data.csv difference-of-squares
