e018304c88e3e2597d8031545caa0874164aed6b
Isaac Good <github@isaacgood.com>
2025-08-26 22:47:44 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - acronym

--- exercises/practice/acronym/create_fixture.sql
+++ exercises/practice/acronym/create_fixture.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS acronym;
-CREATE TABLE acronym (
-  phrase TEXT PRIMARY KEY,
-  result TEXT
-);
+
+CREATE TABLE acronym (phrase TEXT PRIMARY KEY, result TEXT);
 
 .mode csv
 .import ./data.csv acronym
