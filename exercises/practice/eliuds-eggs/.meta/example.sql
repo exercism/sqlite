14737f258b3b3dbc504cbb5daff77b587bb38f3a
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:00 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - eliuds-eggs

--- exercises/practice/eliuds-eggs/.meta/example.sql
+++ exercises/practice/eliuds-eggs/.meta/example.sql
@@ -1,14 +1,39 @@
 UPDATE "eliuds-eggs"
-SET result = 0;
+SET
+  result = 0;
 
 UPDATE "eliuds-eggs"
-SET result = counter FROM
-    (WITH RECURSIVE powers_of_two(x) AS (
-                SELECT 1
-                UNION ALL
-                SELECT x*2 FROM powers_of_two WHERE x<=(SELECT MAX(number) FROM "eliuds-eggs")
-                )
-    SELECT  number, sum(number / (x / 2) % 2) as counter FROM powers_of_two, "eliuds-eggs"
-    WHERE x <= 2*number
-    GROUP BY number) AS calculation
-WHERE calculation.number = "eliuds-eggs".number;
+SET
+  result = counter
+FROM
+  (
+    WITH RECURSIVE
+      powers_of_two (x) AS (
+        SELECT
+          1
+        UNION ALL
+        SELECT
+          x * 2
+        FROM
+          powers_of_two
+        WHERE
+          x <= (
+            SELECT
+              MAX(number)
+            FROM
+              "eliuds-eggs"
+          )
+      )
+    SELECT
+      number,
+      sum(number / (x / 2) % 2) as counter
+    FROM
+      powers_of_two,
+      "eliuds-eggs"
+    WHERE
+      x <= 2 * number
+    GROUP BY
+      number
+  ) AS calculation
+WHERE
+  calculation.number = "eliuds-eggs".number;
