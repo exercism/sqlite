40dbea24f0c0f7afcb81509a9b9fe55cad199733
Isaac Good <github@isaacgood.com>
2025-08-26 22:47:53 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - bob

--- exercises/practice/bob/.meta/example.sql
+++ exercises/practice/bob/.meta/example.sql
@@ -1,27 +1,32 @@
-WITH trimmed AS (
-    SELECT input
-         , TRIM(input, CHAR(32, 9, 10, 13)) AS trimmed
-    FROM bob
-)
-, bools AS (
-    SELECT input
-         , trimmed = '' AS is_silence
-         , trimmed LIKE '%?' AS is_question
-         , trimmed GLOB '*[a-zA-Z]*' AND trimmed = UPPER(trimmed) AS is_yelling
-    FROM trimmed
-)
+WITH
+  trimmed AS (
+    SELECT
+      input,
+      TRIM(input, CHAR(32, 9, 10, 13)) AS trimmed
+    FROM
+      bob
+  ),
+  bools AS (
+    SELECT
+      input,
+      trimmed = '' AS is_silence,
+      trimmed LIKE '%?' AS is_question,
+      trimmed GLOB '*[a-zA-Z]*'
+      AND trimmed = UPPER(trimmed) AS is_yelling
+    FROM
+      trimmed
+  )
 UPDATE bob
-SET reply =
-    CASE
-        WHEN bools.is_silence
-            THEN 'Fine. Be that way!'
-        WHEN bools.is_yelling AND bools.is_question
-            THEN 'Calm down, I know what I''m doing!'
-        WHEN bools.is_yelling
-            THEN 'Whoa, chill out!'
-        WHEN bools.is_question
-            THEN 'Sure.'
-        ELSE 'Whatever.'
-    END
-FROM bools
-WHERE bob.input = bools.input;
+SET
+  reply = CASE
+    WHEN bools.is_silence THEN 'Fine. Be that way!'
+    WHEN bools.is_yelling
+    AND bools.is_question THEN 'Calm down, I know what I''m doing!'
+    WHEN bools.is_yelling THEN 'Whoa, chill out!'
+    WHEN bools.is_question THEN 'Sure.'
+    ELSE 'Whatever.'
+  END
+FROM
+  bools
+WHERE
+  bob.input = bools.input;
