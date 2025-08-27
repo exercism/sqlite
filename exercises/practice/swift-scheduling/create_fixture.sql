9a3d3dd25048130600bb05691081cfad9c22f8db
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:47 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - swift-scheduling

--- exercises/practice/swift-scheduling/create_fixture.sql
+++ exercises/practice/swift-scheduling/create_fixture.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS "swift-scheduling";
+
 CREATE TABLE "swift-scheduling" (
-    meeting_start    TEXT NOT NULL, -- datetime YYYY-MM-DDTHH:mm:ss
-    date_description TEXT NOT NULL,
-    result           TEXT           -- datetime YYYY-MM-DDTHH:mm:ss
+  meeting_start TEXT NOT NULL, -- datetime YYYY-MM-DDTHH:mm:ss
+  date_description TEXT NOT NULL,
+  result TEXT -- datetime YYYY-MM-DDTHH:mm:ss
 );
 
 .mode csv
