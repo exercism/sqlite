2fa338e54f8d93c608f783aa51d73df08925e9ca
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:26 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - raindrops

--- exercises/practice/raindrops/.meta/example.sql
+++ exercises/practice/raindrops/.meta/example.sql
@@ -1,5 +1,27 @@
-UPDATE raindrops SET sound = '';
-UPDATE raindrops SET sound = sound || 'Pling' WHERE number % 3 == 0;
-UPDATE raindrops SET sound = sound || 'Plang' WHERE number % 5 == 0;
-UPDATE raindrops SET sound = sound || 'Plong' WHERE number % 7 == 0;
-UPDATE raindrops SET sound = number WHERE sound = '';
+UPDATE raindrops
+SET
+  sound = '';
+
+UPDATE raindrops
+SET
+  sound = sound || 'Pling'
+WHERE
+  number % 3 == 0;
+
+UPDATE raindrops
+SET
+  sound = sound || 'Plang'
+WHERE
+  number % 5 == 0;
+
+UPDATE raindrops
+SET
+  sound = sound || 'Plong'
+WHERE
+  number % 7 == 0;
+
+UPDATE raindrops
+SET
+  sound = number
+WHERE
+  sound = '';
--- exercises/practice/raindrops/create_fixture.sql
+++ exercises/practice/raindrops/create_fixture.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS raindrops;
-CREATE TABLE "raindrops" (
-    "number" INT, "sound" TEXT
-);
+
+CREATE TABLE "raindrops" ("number" INT, "sound" TEXT);
 
 .mode csv
 .import ./data.csv raindrops
