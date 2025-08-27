65b796b3ac187f9036c6c79c1c784ca7c1b985bc
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:56 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - yacht

--- exercises/practice/yacht/test_reporter.sql
+++ exercises/practice/yacht/test_reporter.sql
@@ -1,20 +1,40 @@
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for ' || tests.dice_results || ' as ' || tests.category
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT  dice_results, category, result FROM yacht) AS actual
-WHERE (actual.dice_results, actual.category) = (tests.dice_results, tests.category) AND tests.status = 'fail';
+SET
+  message = (
+    'Result for ' || tests.dice_results || ' as ' || tests.category || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      dice_results,
+      category,
+      result
+    FROM
+      yacht
+  ) AS actual
+WHERE
+  (actual.dice_results, actual.category) = (tests.dice_results, tests.category)
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT name, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  name,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT name, status, message
-FROM tests;
+SELECT
+  name,
+  status,
+  message
+FROM
+  tests;
--- exercises/practice/yacht/test_setup.sql
+++ exercises/practice/yacht/test_setup.sql
@@ -1,26 +1,25 @@
 -- Create database:
 .read ./create_fixture.sql
-
 -- Read user student solution and save any output as markdown in user_output.md:
 .mode markdown
 .output user_output.md
 .read ./yacht.sql
 .output
-
 -- Create a clean testing environment:
 DROP TABLE IF EXISTS tests;
+
 CREATE TABLE IF NOT EXISTS tests (
-    -- uuid and name (description) are taken from the test.toml file
-    uuid TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    -- The following section is needed by the online test-runner
-    status TEXT DEFAULT 'fail',
-    message TEXT,
-    output TEXT,
-    test_code TEXT,
-    task_id INTEGER DEFAULT NULL,
-    -- Here are columns for the actual tests
-    dice_results TEXT NOT NULL,
-    category TEXT NOT NULL,
-    expected INT NOT NULL
+  -- uuid and name (description) are taken from the test.toml file
+  uuid TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  -- The following section is needed by the online test-runner
+  status TEXT DEFAULT 'fail',
+  message TEXT,
+  output TEXT,
+  test_code TEXT,
+  task_id INTEGER DEFAULT NULL,
+  -- Here are columns for the actual tests
+  dice_results TEXT NOT NULL,
+  category TEXT NOT NULL,
+  expected INT NOT NULL
 );
