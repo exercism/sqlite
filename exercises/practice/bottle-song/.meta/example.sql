5496701b3c64348e79eea5c1ce261a64f7a4de42
Isaac Good <github@isaacgood.com>
2025-08-26 22:47:54 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - bottle-song

--- exercises/practice/bottle-song/.meta/example.sql
+++ exercises/practice/bottle-song/.meta/example.sql
@@ -1,48 +1,74 @@
 DROP TABLE IF EXISTS pairs;
+
 CREATE TEMPORARY TABLE pairs (
-    verse_number INTEGER NOT NULL,
-    start_number TEXT    NOT NULL,
-    next_number  TEXT    NOT NULL
+  verse_number INTEGER NOT NULL,
+  start_number TEXT NOT NULL,
+  next_number TEXT NOT NULL
 );
-INSERT INTO pairs (verse_number, start_number, next_number)
-VALUES (10, 'Ten'  , 'Nine' ),
-       ( 9, 'Nine' , 'Eight'),
-       ( 8, 'Eight', 'Seven'),
-       ( 7, 'Seven', 'Six'  ),
-       ( 6, 'Six'  , 'Five' ),
-       ( 5, 'Five' , 'Four' ),
-       ( 4, 'Four' , 'Three'),
-       ( 3, 'Three', 'Two'  ),
-       ( 2, 'Two'  , 'One'  ),
-       ( 1, 'One'  , 'no'   );
+
+INSERT INTO
+  pairs (verse_number, start_number, next_number)
+VALUES
+  (10, 'Ten', 'Nine'),
+  (9, 'Nine', 'Eight'),
+  (8, 'Eight', 'Seven'),
+  (7, 'Seven', 'Six'),
+  (6, 'Six', 'Five'),
+  (5, 'Five', 'Four'),
+  (4, 'Four', 'Three'),
+  (3, 'Three', 'Two'),
+  (2, 'Two', 'One'),
+  (1, 'One', 'no');
 
 DROP TABLE IF EXISTS verses;
+
 CREATE TABLE verses AS
-  SELECT verse_number,
-         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), start_number) ||
-         PRINTF('%s green bottles hanging on the wall,' || CHAR(10), start_number) ||
-         'And if one green bottle should accidentally fall,' || CHAR(10) ||
-         PRINTF('There''ll be %s green bottles hanging on the wall.',LOWER(next_number)) AS verse
-  FROM pairs
-;
+SELECT
+  verse_number,
+  PRINTF(
+    '%s green bottles hanging on the wall,' || CHAR(10),
+    start_number
+  ) || PRINTF(
+    '%s green bottles hanging on the wall,' || CHAR(10),
+    start_number
+  ) || 'And if one green bottle should accidentally fall,' || CHAR(10) || PRINTF(
+    'There''ll be %s green bottles hanging on the wall.',
+    LOWER(next_number)
+  ) AS verse
+FROM
+  pairs;
 
 UPDATE verses
-   SET verse = REPLACE(verse, 'be one green bottles', 'be one green bottle')
- WHERE verse_number = 2;
-UPDATE verses
-   SET verse = REPLACE(verse, 'One green bottles', 'One green bottle')
- WHERE verse_number = 1;
+SET
+  verse = REPLACE(
+    verse,
+    'be one green bottles',
+    'be one green bottle'
+  )
+WHERE
+  verse_number = 2;
 
+UPDATE verses
+SET
+  verse = REPLACE(verse, 'One green bottles', 'One green bottle')
+WHERE
+  verse_number = 1;
 
 UPDATE "bottle-song"
-   SET result = (
-     SELECT GROUP_CONCAT(verse, CHAR(10)||CHAR(10))
-       FROM (
-         SELECT verse
-           FROM verses
-          WHERE verse_number <= start_bottles
-            AND verse_number > start_bottles - take_down
-          ORDER BY verse_number DESC
-       )
-   )
-;
+SET
+  result = (
+    SELECT
+      GROUP_CONCAT(verse, CHAR(10) || CHAR(10))
+    FROM
+      (
+        SELECT
+          verse
+        FROM
+          verses
+        WHERE
+          verse_number <= start_bottles
+          AND verse_number > start_bottles - take_down
+        ORDER BY
+          verse_number DESC
+      )
+  );
