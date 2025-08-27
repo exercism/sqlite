372cef4674e2050ae5c606f20efcba3328b50660
Isaac Good <github@isaacgood.com>
2025-08-26 22:48:15 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - luhn

--- exercises/practice/luhn/.meta/example.sql
+++ exercises/practice/luhn/.meta/example.sql
@@ -1,42 +1,59 @@
 ALTER TABLE luhn
 ADD luhn_sum INT DEFAULT 0;
+
 ALTER TABLE luhn
 ADD result_string TEXT;
 
-CREATE VIEW cleaned_up (value, invalid_chars, no_spaces)
-AS
-    SELECT 
-        value,
-        value GLOB '*[^0-9 ]*',
-        REPLACE(value, ' ', '')
-    FROM luhn;
+CREATE VIEW cleaned_up (value, invalid_chars, no_spaces) AS
+SELECT
+  value,
+  value GLOB '*[^0-9 ]*',
+  REPLACE(value, ' ', '')
+FROM
+  luhn;
 
 PRAGMA recursive_triggers = ON;
 
-CREATE TRIGGER IF NOT EXISTS update_luhn
-	AFTER UPDATE
-    ON luhn
-    WHEN LENGTH(NEW.result_string) > 0
-BEGIN
-	UPDATE luhn
-    SET luhn_sum = luhn_sum + SUBSTR(NEW.result_string, -1, 1) + IIF(LENGTH(NEW.result_string) > 1,
-                                                                    IIF(SUBSTR(NEW.result_string, -2, 1) > '4',
-                                                                        2 * SUBSTR(NEW.result_string, -2, 1) - 9,
-                                                                        2 * SUBSTR(NEW.result_string, -2, 1)),
-                                                                    0),
-        result_string = SUBSTR(NEW.result_string, 1, LENGTH(NEW.result_string) - 2)
-    WHERE result_string = NEW.result_string;
+CREATE TRIGGER IF NOT EXISTS update_luhn AFTER
+UPDATE ON luhn WHEN LENGTH(NEW.result_string) > 0 BEGIN
+UPDATE luhn
+SET
+  luhn_sum = luhn_sum + SUBSTR(NEW.result_string, -1, 1) + IIF(
+    LENGTH(NEW.result_string) > 1,
+    IIF(
+      SUBSTR(NEW.result_string, -2, 1) > '4',
+      2 * SUBSTR(NEW.result_string, -2, 1) - 9,
+      2 * SUBSTR(NEW.result_string, -2, 1)
+    ),
+    0
+  ),
+  result_string = SUBSTR(
+    NEW.result_string,
+    1,
+    LENGTH(NEW.result_string) - 2
+  )
+WHERE
+  result_string = NEW.result_string;
+
 END;
 
+UPDATE luhn
+SET
+  result = False
+WHERE
+  value GLOB '*[^0-9 ]*'
+  OR LENGTH(REPLACE(value, ' ', '')) <= 1;
 
 UPDATE luhn
-SET result = False
-WHERE value GLOB '*[^0-9 ]*' OR LENGTH(REPLACE(value, ' ', '')) <= 1;
+SET
+  result_string = REPLACE(value, ' ', '')
+WHERE
+  NOT value GLOB '*[^0-9 ]*'
+  AND LENGTH(REPLACE(value, ' ', '')) > 1;
 
 UPDATE luhn
-SET result_string = REPLACE(value, ' ', '')
-WHERE NOT value GLOB '*[^0-9 ]*' AND LENGTH(REPLACE(value, ' ', '')) > 1;
-
-UPDATE luhn
-SET result = (luhn_sum % 10 = 0)
-WHERE NOT value GLOB '*[^0-9 ]*' AND LENGTH(REPLACE(value, ' ', '')) > 1;
+SET
+  result = (luhn_sum % 10 = 0)
+WHERE
+  NOT value GLOB '*[^0-9 ]*'
+  AND LENGTH(REPLACE(value, ' ', '')) > 1;
--- exercises/practice/luhn/test_reporter.sql
+++ exercises/practice/luhn/test_reporter.sql
@@ -1,20 +1,39 @@
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for ' || tests.value
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT value, result FROM luhn) AS actual
-WHERE (actual.value) = (tests.value) AND tests.status = 'fail';
+SET
+  message = (
+    'Result for ' || tests.value || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      value,
+      result
+    FROM
+      luhn
+  ) AS actual
+WHERE
+  (actual.value) = (tests.value)
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT name, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  name,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT name, status, message
-FROM tests;
+SELECT
+  name,
+  status,
+  message
+FROM
+  tests;
