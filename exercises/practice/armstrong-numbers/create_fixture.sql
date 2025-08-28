82f35390dd45efbe0aefbb38395d758f23e70f65
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-27 20:36:27 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - armstrong-numbers (#142)

--- exercises/practice/armstrong-numbers/create_fixture.sql
+++ exercises/practice/armstrong-numbers/create_fixture.sql
@@ -1,10 +1,7 @@
 DROP TABLE IF EXISTS "armstrong-numbers";
-CREATE TABLE "armstrong-numbers" (
-    "number" INT,
-    "result" BOOLEAN
-);
+
+CREATE TABLE "armstrong-numbers" ("number" INT, "result" BOOLEAN);
 
 -- Note: the CSV file may contain literal tab, newline, carriage returns.
-
 .mode csv
 .import ./data.csv armstrong-numbers
