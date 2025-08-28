62c10bcf5d69f9ddd035c89385a1a860c4dc65e1
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-27 20:29:57 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - word-count (#187)

--- exercises/practice/word-count/create_fixture.sql
+++ exercises/practice/word-count/create_fixture.sql
@@ -1,11 +1,16 @@
 DROP TABLE IF EXISTS "word-count";
+
 CREATE TABLE "word-count" (
-    sentence TEXT NOT NULL,
-    result   TEXT               -- json object
+  sentence TEXT NOT NULL,
+  result TEXT -- json object
 );
 
 .mode csv
 .import ./data.csv "word-count"
+UPDATE "word-count"
+SET
+  sentence = REPLACE(sentence, '\n', CHAR(10));
 
-UPDATE "word-count" SET sentence = REPLACE(sentence, '\n', CHAR(10));
-UPDATE "word-count" SET result = NULL;
+UPDATE "word-count"
+SET
+  result = NULL;
