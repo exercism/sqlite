77773ee95532033fd3ad66c13a189cf99f2bbed9
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-08-27 23:17:01 -0300

    * add practice exercise: perfect-numbers

---
+++ exercises/practice/perfect-numbers/create_fixture.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "perfect-numbers";
+CREATE TABLE "perfect-numbers" (
+  number INTEGER NOT NULL,
+  result TEXT,
+  error  TEXT
+);
+
+.mode csv
+.import ./data.csv "perfect-numbers"
+
+UPDATE "perfect-numbers" SET result = NULL, error = NULL;
