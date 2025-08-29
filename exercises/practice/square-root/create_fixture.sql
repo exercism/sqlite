163835c2c4aef14b8b531c7cd5c18d69b151b384
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-28 22:20:03 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - square-root (#179)
    [no important files changed]

--- exercises/practice/square-root/create_fixture.sql
+++ exercises/practice/square-root/create_fixture.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS "square-root";
-CREATE TABLE "square-root" (
-    radicand INTEGER NOT NULL,
-    result   INTEGER
-);
+
+CREATE TABLE "square-root" (radicand INTEGER NOT NULL, result INTEGER);
 
 .mode csv
 .import ./data.csv "square-root"
-
-UPDATE "square-root" SET result = NULL;
+UPDATE "square-root"
+SET
+  result = NULL;
--- exercises/practice/square-root/create_test_table.sql
+++ exercises/practice/square-root/create_test_table.sql
@@ -1,24 +1,56 @@
 DROP TABLE IF EXISTS tests;
+
 CREATE TABLE IF NOT EXISTS tests (
-    -- uuid and description are taken from the test.toml file
-    uuid TEXT PRIMARY KEY,
-    description TEXT NOT NULL,
-    -- The following section is needed by the online test-runner
-    status TEXT DEFAULT 'fail',
-    message TEXT,
-    output TEXT,
-    test_code TEXT,
-    task_id INTEGER DEFAULT NULL,
-    -- Here are columns for the actual tests
-    radicand INTEGER NOT NULL,
-    expected INTEGER NOT NULL
+  -- uuid and description are taken from the test.toml file
+  uuid TEXT PRIMARY KEY,
+  description TEXT NOT NULL,
+  -- The following section is needed by the online test-runner
+  status TEXT DEFAULT 'fail',
+  message TEXT,
+  output TEXT,
+  test_code TEXT,
+  task_id INTEGER DEFAULT NULL,
+  -- Here are columns for the actual tests
+  radicand INTEGER NOT NULL,
+  expected INTEGER NOT NULL
 );
 
-INSERT INTO tests (uuid, description, radicand, expected)
-    VALUES
-        ('9b748478-7b0a-490c-b87a-609dacf631fd', 'root of 1', 1, 1),
-        ('7d3aa9ba-9ac6-4e93-a18b-2e8b477139bb', 'root of 4', 4, 2),
-        ('6624aabf-3659-4ae0-a1c8-25ae7f33c6ef', 'root of 25', 25, 5),
-        ('93beac69-265e-4429-abb1-94506b431f81', 'root of 81', 81, 9),
-        ('fbddfeda-8c4f-4bc4-87ca-6991af35360e', 'root of 196', 196, 14),
-        ('c03d0532-8368-4734-a8e0-f96a9eb7fc1d', 'root of 65025', 65025, 255);
+INSERT INTO
+  tests (uuid, description, radicand, expected)
+VALUES
+  (
+    '9b748478-7b0a-490c-b87a-609dacf631fd',
+    'root of 1',
+    1,
+    1
+  ),
+  (
+    '7d3aa9ba-9ac6-4e93-a18b-2e8b477139bb',
+    'root of 4',
+    4,
+    2
+  ),
+  (
+    '6624aabf-3659-4ae0-a1c8-25ae7f33c6ef',
+    'root of 25',
+    25,
+    5
+  ),
+  (
+    '93beac69-265e-4429-abb1-94506b431f81',
+    'root of 81',
+    81,
+    9
+  ),
+  (
+    'fbddfeda-8c4f-4bc4-87ca-6991af35360e',
+    'root of 196',
+    196,
+    14
+  ),
+  (
+    'c03d0532-8368-4734-a8e0-f96a9eb7fc1d',
+    'root of 65025',
+    65025,
+    255
+  );
