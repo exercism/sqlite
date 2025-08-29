9077a9534db4b9c4ec24755a1c12e48e518b44e3
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-28 22:19:18 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - sublist (#181)
    [no important files changed]

--- exercises/practice/sublist/.meta/example.sql
+++ exercises/practice/sublist/.meta/example.sql
@@ -1,49 +1,107 @@
 UPDATE sublist
-   SET result = 'equal'
- WHERE JSON(list_one) = JSON(list_two)
-;
+SET
+  result = 'equal'
+WHERE
+  JSON(list_one) = JSON(list_two);
 
 UPDATE sublist
-   SET result = (
-     WITH RECURSIVE to_subl (list, i, len, subl) AS (
-       VALUES(list_two, 0, JSON_ARRAY_LENGTH(list_one) - 1, NULL)
-       UNION ALL
-       SELECT list, i + 1, len, (
-         SELECT JSON_GROUP_ARRAY(value)
-           FROM (
-             SELECT j.value
-               FROM json_each(list) j
-              WHERE KEY BETWEEN i AND len + i
-           ))
-         FROM to_subl
-        WHERE i + len < JSON_ARRAY_LENGTH(list)
-     ) SELECT 'sublist' FROM to_subl WHERE JSON(list_one) = JSON(subl) LIMIT 1
-   )
- WHERE NULLIF(result, '') ISNULL
-   AND JSON_ARRAY_LENGTH(list_one) < JSON_ARRAY_LENGTH(list_two)
-;
+SET
+  result = (
+    WITH RECURSIVE
+      to_subl (list, i, len, subl) AS (
+        VALUES
+          (
+            list_two,
+            0,
+            JSON_ARRAY_LENGTH(list_one) - 1,
+            NULL
+          )
+        UNION ALL
+        SELECT
+          list,
+          i + 1,
+          len,
+          (
+            SELECT
+              JSON_GROUP_ARRAY(value)
+            FROM
+              (
+                SELECT
+                  j.value
+                FROM
+                  json_each(list) j
+                WHERE
+                  KEY BETWEEN i AND len  + i
+              )
+          )
+        FROM
+          to_subl
+        WHERE
+          i + len < JSON_ARRAY_LENGTH(list)
+      )
+    SELECT
+      'sublist'
+    FROM
+      to_subl
+    WHERE
+      JSON(list_one) = JSON(subl)
+    LIMIT
+      1
+  )
+WHERE
+  NULLIF(result, '') ISNULL
+  AND JSON_ARRAY_LENGTH(list_one) < JSON_ARRAY_LENGTH(list_two);
 
 UPDATE sublist
-   SET result = (
-     WITH RECURSIVE to_subl (list, i, len, subl) AS (
-       VALUES(list_one, 0, JSON_ARRAY_LENGTH(list_two) - 1, NULL)
-       UNION ALL
-       SELECT list, i + 1, len, (
-         SELECT JSON_GROUP_ARRAY(value)
-           FROM (
-             SELECT j.value
-               FROM json_each(list) j
-              WHERE KEY BETWEEN i AND len + i
-           ))
-         FROM to_subl
-        WHERE i + len < JSON_ARRAY_LENGTH(list)
-     ) SELECT 'superlist' FROM to_subl WHERE JSON(list_two) = JSON(subl) LIMIT 1
-   )
- WHERE NULLIF(result, '') ISNULL
-   AND JSON_ARRAY_LENGTH(list_one) > JSON_ARRAY_LENGTH(list_two)
-;
+SET
+  result = (
+    WITH RECURSIVE
+      to_subl (list, i, len, subl) AS (
+        VALUES
+          (
+            list_one,
+            0,
+            JSON_ARRAY_LENGTH(list_two) - 1,
+            NULL
+          )
+        UNION ALL
+        SELECT
+          list,
+          i + 1,
+          len,
+          (
+            SELECT
+              JSON_GROUP_ARRAY(value)
+            FROM
+              (
+                SELECT
+                  j.value
+                FROM
+                  json_each(list) j
+                WHERE
+                  KEY BETWEEN i AND len  + i
+              )
+          )
+        FROM
+          to_subl
+        WHERE
+          i + len < JSON_ARRAY_LENGTH(list)
+      )
+    SELECT
+      'superlist'
+    FROM
+      to_subl
+    WHERE
+      JSON(list_two) = JSON(subl)
+    LIMIT
+      1
+  )
+WHERE
+  NULLIF(result, '') ISNULL
+  AND JSON_ARRAY_LENGTH(list_one) > JSON_ARRAY_LENGTH(list_two);
 
 UPDATE SUBLIST
-   SET result = 'unequal'
- WHERE NULLIF(result, '') ISNULL
-;
+SET
+  result = 'unequal'
+WHERE
+  NULLIF(result, '') ISNULL;
