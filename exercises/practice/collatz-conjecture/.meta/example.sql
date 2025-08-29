58108ba42a97988a7ccb2c6fa4b5254794d211de
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-28 22:21:43 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - collatz-conjecture (#146)
    [no important files changed]

--- exercises/practice/collatz-conjecture/.meta/example.sql
+++ exercises/practice/collatz-conjecture/.meta/example.sql
@@ -2,20 +2,26 @@ ALTER TABLE collatz
 ADD current INTEGER;
 
 UPDATE collatz
-SET steps = 0;
+SET
+  steps = 0;
 
 PRAGMA recursive_triggers = ON;
 
-CREATE TRIGGER IF NOT EXISTS update_collatz
-	AFTER UPDATE
-    ON collatz
-    WHEN NEW.current != 1
-BEGIN
-	UPDATE collatz
-    SET current = IIF(NEW.current % 2 == 0, NEW.current / 2, NEW.current * 3 + 1),
-        steps = NEW.steps + 1
-    WHERE current = NEW.current;
+CREATE TRIGGER IF NOT EXISTS update_collatz AFTER
+UPDATE ON collatz WHEN NEW.current != 1 BEGIN
+UPDATE collatz
+SET
+  current = IIF(
+    NEW.current % 2 == 0,
+    NEW.current / 2,
+    NEW.current * 3 + 1
+  ),
+  steps = NEW.steps + 1
+WHERE
+  current = NEW.current;
+
 END;
 
 UPDATE collatz
-SET current = number;
+SET
+  current = number;
--- exercises/practice/collatz-conjecture/create_fixture.sql
+++ exercises/practice/collatz-conjecture/create_fixture.sql
@@ -1,10 +1,7 @@
 DROP TABLE IF EXISTS collatz;
-CREATE TABLE "collatz" (
-    "number" INTEGER,
-    "steps" INTEGER
-);
+
+CREATE TABLE "collatz" ("number" INTEGER, "steps" INTEGER);
 
 -- Note: the CSV file contain literal tab, newline, carriage returns.
-
 .mode csv
 .import ./data.csv collatz
