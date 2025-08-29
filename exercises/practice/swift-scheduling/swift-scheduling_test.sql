b8ec2d1f4c6ef46387f3dd1589bb1ef2413e077b
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-28 22:18:46 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - swift-scheduling (#182)
    [no important files changed]

--- exercises/practice/swift-scheduling/swift-scheduling_test.sql
+++ exercises/practice/swift-scheduling/swift-scheduling_test.sql
@@ -1,40 +1,73 @@
 -- Create database:
 .read ./create_fixture.sql
-
 -- Read user student solution and save any output as markdown in user_output.md:
 .mode markdown
 .output user_output.md
 .read ./swift-scheduling.sql
 .output
-
 -- Create a clean testing environment:
 .read ./create_test_table.sql
-
 -- Comparison of user input and the tests updates the status for each test:
 UPDATE tests
-SET status = 'pass'
-FROM (SELECT meeting_start, date_description, result FROM "swift-scheduling") AS actual
-WHERE (actual.meeting_start, actual.date_description, actual.result) = (tests.meeting_start, tests.date_description, tests.expected);
+SET
+  status = 'pass'
+FROM
+  (
+    SELECT
+      meeting_start,
+      date_description,
+      result
+    FROM
+      "swift-scheduling"
+  ) AS actual
+WHERE
+  (
+    actual.meeting_start,
+    actual.date_description,
+    actual.result
+  ) = (
+    tests.meeting_start,
+    tests.date_description,
+    tests.expected
+  );
 
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for "'
-    || tests.meeting_start || ', ' || tests.date_description
-    || '"'
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT meeting_start, date_description, result FROM "swift-scheduling") AS actual
-WHERE (actual.meeting_start, actual.date_description) = (tests.meeting_start, tests.date_description) AND tests.status = 'fail';
+SET
+  message = (
+    'Result for "' || tests.meeting_start || ', ' || tests.date_description || '"' || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      meeting_start,
+      date_description,
+      result
+    FROM
+      "swift-scheduling"
+  ) AS actual
+WHERE
+  (actual.meeting_start, actual.date_description) = (tests.meeting_start, tests.date_description)
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT description, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  description,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT description, status, message
-FROM tests;
+SELECT
+  description,
+  status,
+  message
+FROM
+  tests;
