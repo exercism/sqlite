13fc5208dd80cf22c984cdbecb78452118e4aa6c
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:24:10 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - pangram (#164)
    [no important files changed]

--- exercises/practice/pangram/create_fixture.sql
+++ exercises/practice/pangram/create_fixture.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS pangram;
-CREATE TABLE pangram (
-    sentence TEXT    NOT NULL,
-    result   BOOLEAN
-);
+
+CREATE TABLE pangram (sentence TEXT NOT NULL, result BOOLEAN);
 
 .mode csv
 .import ./data.csv pangram
-
-UPDATE pangram SET result = NULL;
+UPDATE pangram
+SET
+  result = NULL;
