5614f4348778bdc68a67b70d9d74a2870962123a
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:23:46 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - meetup (#162)
    [no important files changed]

--- exercises/practice/meetup/create_fixture.sql
+++ exercises/practice/meetup/create_fixture.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS meetup;
+
 CREATE TABLE "meetup" (
-    "year" INTEGER,
-    "month" INTEGER,
-    "week" TEXT,
-    "dayofweek" TEXT,
-    "result" TEXT
+  "year" INTEGER,
+  "month" INTEGER,
+  "week" TEXT,
+  "dayofweek" TEXT,
+  "result" TEXT
 );
 
 -- Note: the CSV file contain literal tab, newline, carriage returns.
-
 .mode csv
 .import ./data.csv meetup
--- exercises/practice/meetup/test_reporter.sql
+++ exercises/practice/meetup/test_reporter.sql
@@ -1,20 +1,52 @@
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for ' || tests.year || '/' || tests.month || ', ' || tests.week || ' ' || tests.dayofweek
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT year, month, week, dayofweek, result  FROM meetup) AS actual
-WHERE (actual.year, actual.month, actual.week, actual.dayofweek) = (tests.year, tests.month, tests.week, tests.dayofweek) AND tests.status = 'fail';
+SET
+  message = (
+    'Result for ' || tests.year || '/' || tests.month || ', ' || tests.week || ' ' || tests.dayofweek || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      year,
+      month,
+      week,
+      dayofweek,
+      result
+    FROM
+      meetup
+  ) AS actual
+WHERE
+  (
+    actual.year,
+    actual.month,
+    actual.week,
+    actual.dayofweek
+  ) = (
+    tests.year,
+    tests.month,
+    tests.week,
+    tests.dayofweek
+  )
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT name, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  name,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT name, status, message
-FROM tests;
+SELECT
+  name,
+  status,
+  message
+FROM
+  tests;
