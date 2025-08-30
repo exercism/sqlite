ae20c4ef5d9049b3f88129089c66a7c67d6101cb
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 18:51:01 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - scrabble-score (#175)
    [no important files changed]

--- exercises/practice/scrabble-score/create_fixture.sql
+++ exercises/practice/scrabble-score/create_fixture.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS "scrabble-score";
-CREATE TABLE "scrabble-score" (
-    word   TEXT    NOT NULL,
-    result INTEGER
-);
+
+CREATE TABLE "scrabble-score" (word TEXT NOT NULL, result INTEGER);
 
 .mode csv
 .import ./data.csv "scrabble-score"
-
-UPDATE "scrabble-score" SET result = NULL;
+UPDATE "scrabble-score"
+SET
+  result = NULL;
