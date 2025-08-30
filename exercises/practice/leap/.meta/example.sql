e81c8a3182a4ff233e219100a7e548b68267c74b
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:48:01 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - leap (#159)
    [no important files changed]

--- exercises/practice/leap/.meta/example.sql
+++ exercises/practice/leap/.meta/example.sql
@@ -1,3 +1,9 @@
 UPDATE leap
-SET is_leap = 1
-WHERE year % 4 = 0  AND (year % 100 != 0 OR year % 400 = 0);
+SET
+  is_leap = 1
+WHERE
+  year % 4 = 0
+  AND (
+    year % 100 != 0
+    OR year % 400 = 0
+  );
