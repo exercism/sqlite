79306453239c24ecd191fbab1109db8e9149f328
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 18:50:32 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - roman-numerals (#174)
    [no important files changed]

--- exercises/practice/roman-numerals/.meta/example.sql
+++ exercises/practice/roman-numerals/.meta/example.sql
@@ -3,33 +3,44 @@ ADD temp_num TEXT DEFAULT '';
 
 PRAGMA recursive_triggers = ON;
 
-CREATE TRIGGER IF NOT EXISTS update_roman_numerals
-    AFTER UPDATE
-    ON "roman-numerals"
-    WHEN LENGTH(NEW.temp_num) > 0
-BEGIN
-    UPDATE "roman-numerals"
-    SET result =
-        -- we encode the roman numerals in base 10
-        -- to add a digit, we translate IVXLC => XLCDM (multiply by ten)
-        -- and then add the digit at the end
+CREATE TRIGGER IF NOT EXISTS update_roman_numerals AFTER
+UPDATE ON "roman-numerals" WHEN LENGTH(NEW.temp_num) > 0 BEGIN
+UPDATE "roman-numerals"
+SET
+  result =
+  -- we encode the roman numerals in base 10
+  -- to add a digit, we translate IVXLC => XLCDM (multiply by ten)
+  -- and then add the digit at the end
+  REPLACE(
+    REPLACE(
+      REPLACE(
+        REPLACE(REPLACE(result, 'C', 'M'), 'L', 'D'),
+        'X',
+        'C'
+      ),
+      'V',
+      'L'
+    ),
+    'I',
+    'X'
+  ) || CASE SUBSTR(temp_num, 1, 1)
+    WHEN '0' THEN ''
+    WHEN '1' THEN 'I'
+    WHEN '2' THEN 'II'
+    WHEN '3' THEN 'III'
+    WHEN '4' THEN 'IV'
+    WHEN '5' THEN 'V'
+    WHEN '6' THEN 'VI'
+    WHEN '7' THEN 'VII'
+    WHEN '8' THEN 'VIII'
+    WHEN '9' THEN 'IX'
+  END,
+  temp_num = SUBSTR(temp_num, 2)
+WHERE
+  temp_num = NEW.temp_num;
 
-        REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(result, 'C', 'M'), 'L', 'D'), 'X', 'C'), 'V', 'L'), 'I', 'X') ||
-        CASE SUBSTR(temp_num, 1, 1)
-        WHEN '0' THEN ''
-        WHEN '1' THEN 'I'
-        WHEN '2' THEN 'II'
-        WHEN '3' THEN 'III'
-        WHEN '4' THEN 'IV'
-        WHEN '5' THEN 'V'
-        WHEN '6' THEN 'VI'
-        WHEN '7' THEN 'VII'
-        WHEN '8' THEN 'VIII'
-        WHEN '9' THEN 'IX'
-        END,
-        temp_num = SUBSTR(temp_num, 2)
-    WHERE temp_num = NEW.temp_num;
 END;
 
 UPDATE "roman-numerals"
-SET temp_num = number || '';
+SET
+  temp_num = number || '';
--- exercises/practice/roman-numerals/create_fixture.sql
+++ exercises/practice/roman-numerals/create_fixture.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS "roman-numerals";
-CREATE TABLE "roman-numerals" (
-    "number" INT,
-    "result" TEXT
-);
+
+CREATE TABLE "roman-numerals" ("number" INT, "result" TEXT);
 
 .mode csv
 .import ./data.csv roman-numerals
