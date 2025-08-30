b4ed692ff120cdb4605542199dc96213965e75b5
Isaac Good <github@isaacgood.com>
2025-08-29 23:08:56 -0700

    Remove note about whitespace in CSV files where the CSV files do not contain whitespace values

--- exercises/practice/armstrong-numbers/create_fixture.sql
+++ exercises/practice/armstrong-numbers/create_fixture.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS "armstrong-numbers";
 
 CREATE TABLE "armstrong-numbers" ("number" INT, "result" BOOLEAN);
 
--- Note: the CSV file may contain literal tab, newline, carriage returns.
 .mode csv
 .import ./data.csv armstrong-numbers
