8ce9509ceea4e64c0f94078f6acc7e6994b884ab
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:56:31 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - isbn-verifier (#156)
    [no important files changed]

--- exercises/practice/isbn-verifier/.meta/example.sql
+++ exercises/practice/isbn-verifier/.meta/example.sql
@@ -1,27 +1,20 @@
 UPDATE "isbn-verifier"
-   SET result = FALSE
- WHERE GLOB(
-         '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9X]',
-         REPLACE(isbn, '-', '')
-       ) = 0
-;
+SET
+  result = FALSE
+WHERE
+  GLOB(
+    '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9X]',
+    REPLACE(isbn, '-', '')
+  ) = 0;
 
 UPDATE "isbn-verifier"
-   SET result = (
-     SUBSTRING(REPLACE(isbn,'-', ''), -10, 1) * 10 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -9, 1) *  9 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -8, 1) *  8 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -7, 1) *  7 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -6, 1) *  6 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -5, 1) *  5 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -4, 1) *  4 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -3, 1) *  3 +
-     SUBSTRING(REPLACE(isbn,'-', ''),  -2, 1) *  2 +
-     IIF(
-       SUBSTRING(REPLACE(isbn,'-', ''), -1, 1) = 'X',
-       10,
-       SUBSTRING(REPLACE(isbn,'-', ''), -1, 1)
-     )
-   ) % 11 = 0
- WHERE result ISNULL
-;
+SET
+  result = (
+    SUBSTRING(REPLACE(isbn, '-', ''), -10, 1) * 10 + SUBSTRING(REPLACE(isbn, '-', ''), -9, 1) * 9 + SUBSTRING(REPLACE(isbn, '-', ''), -8, 1) * 8 + SUBSTRING(REPLACE(isbn, '-', ''), -7, 1) * 7 + SUBSTRING(REPLACE(isbn, '-', ''), -6, 1) * 6 + SUBSTRING(REPLACE(isbn, '-', ''), -5, 1) * 5 + SUBSTRING(REPLACE(isbn, '-', ''), -4, 1) * 4 + SUBSTRING(REPLACE(isbn, '-', ''), -3, 1) * 3 + SUBSTRING(REPLACE(isbn, '-', ''), -2, 1) * 2 + IIF(
+      SUBSTRING(REPLACE(isbn, '-', ''), -1, 1) = 'X',
+      10,
+      SUBSTRING(REPLACE(isbn, '-', ''), -1, 1)
+    )
+  ) % 11 = 0
+WHERE
+  result ISNULL;
