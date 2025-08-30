309f1839f1c67f1f17d1c3a10a398cb4400fbf66
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:48:26 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - kindergarten-garden (#158)
    [no important files changed]

--- exercises/practice/kindergarten-garden/create_fixture.sql
+++ exercises/practice/kindergarten-garden/create_fixture.sql
@@ -1,9 +1,6 @@
 DROP TABLE IF EXISTS "kindergarten-garden";
-CREATE TABLE "kindergarten-garden" (
-    "diagram" TEXT,
-    "student" TEXT,
-    "result" TEXT
-);
+
+CREATE TABLE "kindergarten-garden" ("diagram" TEXT, "student" TEXT, "result" TEXT);
 
 .mode csv
 .import ./data.csv kindergarten-garden
--- exercises/practice/kindergarten-garden/test_reporter.sql
+++ exercises/practice/kindergarten-garden/test_reporter.sql
@@ -1,20 +1,40 @@
 -- Update message for failed tests to give helpful information:
 UPDATE tests
-SET message = (
-    'Result for "' || tests.diagram || '" and "' || tests.student || '"'
-    || ' is <' || COALESCE(actual.result, 'NULL')
-    || '> but should be <' || tests.expected || '>'
-)
-FROM (SELECT diagram, student, result FROM 'kindergarten-garden') AS actual
-WHERE (actual.diagram, actual.student) = (tests.diagram, tests.student) AND tests.status = 'fail';
+SET
+  message = (
+    'Result for "' || tests.diagram || '" and "' || tests.student || '"' || ' is <' || COALESCE(actual.result, 'NULL') || '> but should be <' || tests.expected || '>'
+  )
+FROM
+  (
+    SELECT
+      diagram,
+      student,
+      result
+    FROM
+      'kindergarten-garden'
+  ) AS actual
+WHERE
+  (actual.diagram, actual.student) = (tests.diagram, tests.student)
+  AND tests.status = 'fail';
 
 -- Save results to ./output.json (needed by the online test-runner)
 .mode json
 .once './output.json'
-SELECT name, status, message, output, test_code, task_id
-FROM tests;
+SELECT
+  name,
+  status,
+  message,
+  output,
+  test_code,
+  task_id
+FROM
+  tests;
 
 -- Display test results in readable form for the student:
 .mode table
-SELECT name, status, message
-FROM tests;
+SELECT
+  name,
+  status,
+  message
+FROM
+  tests;
