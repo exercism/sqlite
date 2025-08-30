ca9f449f674331a573389395f5f49e6e30923bfa
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:47:10 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - matching-brackets (#161)
    [no important files changed]

--- exercises/practice/matching-brackets/.meta/example.sql
+++ exercises/practice/matching-brackets/.meta/example.sql
@@ -3,29 +3,44 @@ ADD brackets TEXT DEFAULT '';
 
 PRAGMA recursive_triggers = ON;
 
-CREATE TRIGGER IF NOT EXISTS update_backets
-    AFTER UPDATE
-    ON "matching-brackets"
-    WHEN NEW.brackets != OLD.brackets
-BEGIN
-    UPDATE "matching-brackets"
-    SET brackets = REPLACE(REPLACE(REPLACE(brackets, '()', ''), '[]', ''), '{}', '')
-    WHERE brackets = NEW.brackets;
+CREATE TRIGGER IF NOT EXISTS update_backets AFTER
+UPDATE ON "matching-brackets" WHEN NEW.brackets != OLD.brackets BEGIN
+UPDATE "matching-brackets"
+SET
+  brackets = REPLACE(
+    REPLACE(REPLACE(brackets, '()', ''), '[]', ''),
+    '{}',
+    ''
+  )
+WHERE
+  brackets = NEW.brackets;
+
 END;
 
 UPDATE "matching-brackets"
-SET brackets = (
-    WITH cnt(n) AS (
-        SELECT 1
+SET
+  brackets = (
+    WITH
+      cnt (n) AS (
+        SELECT
+          1
         UNION ALL
-        SELECT n + 1 FROM cnt WHERE n <= LENGTH(input)
-    )
-
-    -- clean the input to only have ()[]{} characters
-    SELECT group_concat(SUBSTR(input, n, 1), '')
-    FROM cnt
-    WHERE '()[]{}' LIKE ('%' || SUBSTR(input, n, 1) || '%')
-);
+        SELECT
+          n + 1
+        FROM
+          cnt
+        WHERE
+          n <= LENGTH(input)
+      )
+      -- clean the input to only have ()[]{} characters
+    SELECT
+      group_concat(SUBSTR(input, n, 1), '')
+    FROM
+      cnt
+    WHERE
+      '()[]{}' LIKE('%' || SUBSTR(input, n, 1) || '%')
+  );
 
 UPDATE "matching-brackets"
-SET result = (brackets = '');
+SET
+  result = (brackets = '');
