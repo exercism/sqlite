ffcf23704da46940450c8d6275ef103490cdd72f
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:21:27 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - reverse-string (#172)
    [no important files changed]

--- exercises/practice/reverse-string/.meta/example.sql
+++ exercises/practice/reverse-string/.meta/example.sql
@@ -1,12 +1,21 @@
 UPDATE "reverse-string"
-   SET result = (
-     WITH RECURSIVE reverse_char(string, char) AS (
-       VALUES(input, '')
-       UNION ALL
-       SELECT SUBSTRING(string, 1, LENGTH(string) - 1),
-              SUBSTRING(string, -1)
-         FROM reverse_char
-        WHERE string <> ''
-     ) SELECT GROUP_CONCAT(char, '') FROM reverse_char
-);
-
+SET
+  result = (
+    WITH RECURSIVE
+      reverse_char (string, char) AS (
+        VALUES
+          (input, '')
+        UNION ALL
+        SELECT
+          SUBSTRING(string, 1, LENGTH(string) - 1),
+          SUBSTRING(string, -1)
+        FROM
+          reverse_char
+        WHERE
+          string <> ''
+      )
+    SELECT
+      GROUP_CONCAT(char, '')
+    FROM
+      reverse_char
+  );
--- exercises/practice/reverse-string/create_fixture.sql
+++ exercises/practice/reverse-string/create_fixture.sql
@@ -1,10 +1,9 @@
 DROP TABLE IF EXISTS "reverse-string";
-CREATE TABLE "reverse-string" (
-    input  TEXT NOT NULL,
-    result TEXT
-);
+
+CREATE TABLE "reverse-string" (input TEXT NOT NULL, result TEXT);
 
 .mode csv
 .import ./data.csv reverse-string
-
-UPDATE "reverse-string" SET result = NULL;
+UPDATE "reverse-string"
+SET
+  result = NULL;
