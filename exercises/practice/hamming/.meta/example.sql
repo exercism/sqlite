341b0dfde7c47f7bee27a3509d33662b93dd1fe6
Isaac Good <IsaacG@users.noreply.github.com>
2025-08-29 22:54:40 -0700

    Auto-format .sql files using github.com/sql-formatter-org/sql-formatter - hamming (#153)
    [no important files changed]

--- exercises/practice/hamming/.meta/example.sql
+++ exercises/practice/hamming/.meta/example.sql
@@ -1,21 +1,33 @@
 UPDATE hamming
-   SET error = 'strands must be of equal length'
- WHERE LENGTH(strand1) != LENGTH(strand2)
-;
+SET
+  error = 'strands must be of equal length'
+WHERE
+  LENGTH(strand1) != LENGTH(strand2);
 
 UPDATE hamming
-   SET result = (
-     WITH RECURSIVE rcte(string1, string2, char1, char2) AS (
-       VALUES(strand1, strand2, '', '')
-       UNION ALL
-       SELECT SUBSTRING(string1, 2),    SUBSTRING(string2, 2),
-              SUBSTRING(string1, 1, 1), SUBSTRING(string2, 1, 1)
-         FROM rcte
-        WHERE string1 <> ''
-     )
-     SELECT COUNT(*)
-       FROM rcte
-      WHERE char1 != char2
-   )
- WHERE LENGTH(strand1) = LENGTH(strand2)
-;
+SET
+  result = (
+    WITH RECURSIVE
+      rcte (string1, string2, char1, char2) AS (
+        VALUES
+          (strand1, strand2, '', '')
+        UNION ALL
+        SELECT
+          SUBSTRING(string1, 2),
+          SUBSTRING(string2, 2),
+          SUBSTRING(string1, 1, 1),
+          SUBSTRING(string2, 1, 1)
+        FROM
+          rcte
+        WHERE
+          string1 <> ''
+      )
+    SELECT
+      COUNT(*)
+    FROM
+      rcte
+    WHERE
+      char1 != char2
+  )
+WHERE
+  LENGTH(strand1) = LENGTH(strand2);
