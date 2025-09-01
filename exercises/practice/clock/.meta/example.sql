4bc79e7eed45b7962bea2758e687a461d70e7396
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2025-09-01 09:41:37 -0300

    * '">>": syntax error', changing '->>' to '->'

--- exercises/practice/clock/.meta/example.sql
+++ exercises/practice/clock/.meta/example.sql
@@ -3,8 +3,8 @@ UPDATE clock
        STRFTIME(
          '%H:%M',
          TIME('00:00',
-              PRINTF('%d HOUR',   input->>'hour'),
-              PRINTF('%d MINUTE', input->>'minute')
+              PRINTF('%d HOUR',   input->'hour'),
+              PRINTF('%d MINUTE', input->'minute')
          )
        )
  WHERE property = 'create'
@@ -15,9 +15,9 @@ UPDATE clock
        STRFTIME('%H:%M',
                 TIME(
                   '00:00',
-                  PRINTF('%d HOUR',    input->>'hour'),
-                  PRINTF('%d minute',  input->>'minute'),
-                  PRINTF('+%d minute', input->>'value')
+                  PRINTF('%d HOUR',    input->'hour'),
+                  PRINTF('%d minute',  input->'minute'),
+                  PRINTF('+%d minute', input->'value')
                 )
        )
  WHERE property = 'add'
@@ -28,9 +28,9 @@ UPDATE clock
        STRFTIME('%H:%M',
                 TIME(
                   '00:00',
-                  PRINTF('%d HOUR',    input->>'hour'),
-                  PRINTF('%d minute',  input->>'minute'),
-                  PRINTF('-%d minute', input->>'value')
+                  PRINTF('%d HOUR',    input->'hour'),
+                  PRINTF('%d minute',  input->'minute'),
+                  PRINTF('-%d minute', input->'value')
                 )
        )
  WHERE property = 'subtract'
@@ -39,11 +39,11 @@ UPDATE clock
 UPDATE clock
    SET result =
        TIME('00:00',
-            PRINTF('%d HOUR',   input->'clock1'->>'hour'),
-            PRINTF('%d MINUTE', input->'clock1'->>'minute')
+            PRINTF('%d HOUR',   input->'clock1'->'hour'),
+            PRINTF('%d MINUTE', input->'clock1'->'minute')
        ) =
        TIME('00:00',
-            PRINTF('%d HOUR',   input->'clock2'->>'hour'),
-            PRINTF('%d MINUTE', input->'clock2'->>'minute')
+            PRINTF('%d HOUR',   input->'clock2'->'hour'),
+            PRINTF('%d MINUTE', input->'clock2'->'minute')
        )
  WHERE property = 'equal';
