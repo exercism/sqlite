3133f1d9f92e21e2c7b41a9fb798899616dc6e4e
Isaac Good <IsaacG@users.noreply.github.com>
2026-08-29 10:34:02 -0700

    Add test generators for `tournament transpose triangletwelve-days two-fer word-count wordy yacht` (#256)

--- exercises/practice/tournament/create_test_table.sql
+++ exercises/practice/tournament/create_test_table.sql
@@ -28,67 +28,117 @@ VALUES
     '1b4a8aef-0734-4007-80a2-0626178c88f4',
     'a win is three points, a loss is zero points',
     'Allegoric Alaskans;Blithering Badgers;win',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  1 |  1 |  0 |  0 |  3\nBlithering Badgers             |  1 |  0 |  0 |  1 |  0'
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
+Blithering Badgers             |  1 |  0 |  0 |  1 |  0'
   ),
   (
     '5f45ac09-4efe-46e7-8ddb-75ad85f86e05',
     'a win can also be expressed as a loss',
     'Blithering Badgers;Allegoric Alaskans;loss',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  1 |  1 |  0 |  0 |  3\nBlithering Badgers             |  1 |  0 |  0 |  1 |  0'
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  1 |  1 |  0 |  0 |  3
+Blithering Badgers             |  1 |  0 |  0 |  1 |  0'
   ),
   (
     'fd297368-efa0-442d-9f37-dd3f9a437239',
     'a different team can win',
     'Blithering Badgers;Allegoric Alaskans;win',
-    'Team                           | MP |  W |  D |  L |  P\nBlithering Badgers             |  1 |  1 |  0 |  0 |  3\nAllegoric Alaskans             |  1 |  0 |  0 |  1 |  0'
+    'Team                           | MP |  W |  D |  L |  P
+Blithering Badgers             |  1 |  1 |  0 |  0 |  3
+Allegoric Alaskans             |  1 |  0 |  0 |  1 |  0'
   ),
   (
     '26c016f9-e753-4a93-94e9-842f7b4d70fc',
     'a draw is one point each',
     'Allegoric Alaskans;Blithering Badgers;draw',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  1 |  0 |  1 |  0 |  1\nBlithering Badgers             |  1 |  0 |  1 |  0 |  1'
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  1 |  0 |  1 |  0 |  1
+Blithering Badgers             |  1 |  0 |  1 |  0 |  1'
   ),
   (
     '731204f6-4f34-4928-97eb-1c307ba83e62',
     'There can be more than one match',
-    'Allegoric Alaskans;Blithering Badgers;win\nAllegoric Alaskans;Blithering Badgers;win',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  2 |  2 |  0 |  0 |  6\nBlithering Badgers             |  2 |  0 |  0 |  2 |  0'
+    'Allegoric Alaskans;Blithering Badgers;win
+Allegoric Alaskans;Blithering Badgers;win',
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6
+Blithering Badgers             |  2 |  0 |  0 |  2 |  0'
   ),
   (
     '49dc2463-42af-4ea6-95dc-f06cc5776adf',
     'There can be more than one winner',
-    'Allegoric Alaskans;Blithering Badgers;loss\nAllegoric Alaskans;Blithering Badgers;win',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  2 |  1 |  0 |  1 |  3\nBlithering Badgers             |  2 |  1 |  0 |  1 |  3'
+    'Allegoric Alaskans;Blithering Badgers;loss
+Allegoric Alaskans;Blithering Badgers;win',
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  2 |  1 |  0 |  1 |  3
+Blithering Badgers             |  2 |  1 |  0 |  1 |  3'
   ),
   (
     '6d930f33-435c-4e6f-9e2d-63fa85ce7dc7',
     'There can be more than two teams',
-    'Allegoric Alaskans;Blithering Badgers;win\nBlithering Badgers;Courageous Californians;win\nCourageous Californians;Allegoric Alaskans;loss',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  2 |  2 |  0 |  0 |  6\nBlithering Badgers             |  2 |  1 |  0 |  1 |  3\nCourageous Californians        |  2 |  0 |  0 |  2 |  0'
+    'Allegoric Alaskans;Blithering Badgers;win
+Blithering Badgers;Courageous Californians;win
+Courageous Californians;Allegoric Alaskans;loss',
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  2 |  2 |  0 |  0 |  6
+Blithering Badgers             |  2 |  1 |  0 |  1 |  3
+Courageous Californians        |  2 |  0 |  0 |  2 |  0'
   ),
   (
     '97022974-0c8a-4a50-8fe7-e36bdd8a5945',
     'typical input',
-    'Allegoric Alaskans;Blithering Badgers;win\nDevastating Donkeys;Courageous Californians;draw\nDevastating Donkeys;Allegoric Alaskans;win\nCourageous Californians;Blithering Badgers;loss\nBlithering Badgers;Devastating Donkeys;loss\nAllegoric Alaskans;Courageous Californians;win',
-    'Team                           | MP |  W |  D |  L |  P\nDevastating Donkeys            |  3 |  2 |  1 |  0 |  7\nAllegoric Alaskans             |  3 |  2 |  0 |  1 |  6\nBlithering Badgers             |  3 |  1 |  0 |  2 |  3\nCourageous Californians        |  3 |  0 |  1 |  2 |  1'
+    'Allegoric Alaskans;Blithering Badgers;win
+Devastating Donkeys;Courageous Californians;draw
+Devastating Donkeys;Allegoric Alaskans;win
+Courageous Californians;Blithering Badgers;loss
+Blithering Badgers;Devastating Donkeys;loss
+Allegoric Alaskans;Courageous Californians;win',
+    'Team                           | MP |  W |  D |  L |  P
+Devastating Donkeys            |  3 |  2 |  1 |  0 |  7
+Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6
+Blithering Badgers             |  3 |  1 |  0 |  2 |  3
+Courageous Californians        |  3 |  0 |  1 |  2 |  1'
   ),
   (
     'fe562f0d-ac0a-4c62-b9c9-44ee3236392b',
     'incomplete competition (not all pairs have played)',
-    'Allegoric Alaskans;Blithering Badgers;loss\nDevastating Donkeys;Allegoric Alaskans;loss\nCourageous Californians;Blithering Badgers;draw\nAllegoric Alaskans;Courageous Californians;win',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  3 |  2 |  0 |  1 |  6\nBlithering Badgers             |  2 |  1 |  1 |  0 |  4\nCourageous Californians        |  2 |  0 |  1 |  1 |  1\nDevastating Donkeys            |  1 |  0 |  0 |  1 |  0'
+    'Allegoric Alaskans;Blithering Badgers;loss
+Devastating Donkeys;Allegoric Alaskans;loss
+Courageous Californians;Blithering Badgers;draw
+Allegoric Alaskans;Courageous Californians;win',
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  3 |  2 |  0 |  1 |  6
+Blithering Badgers             |  2 |  1 |  1 |  0 |  4
+Courageous Californians        |  2 |  0 |  1 |  1 |  1
+Devastating Donkeys            |  1 |  0 |  0 |  1 |  0'
   ),
   (
     '3aa0386f-150b-4f99-90bb-5195e7b7d3b8',
     'ties broken alphabetically',
-    'Courageous Californians;Devastating Donkeys;win\nAllegoric Alaskans;Blithering Badgers;win\nDevastating Donkeys;Allegoric Alaskans;loss\nCourageous Californians;Blithering Badgers;win\nBlithering Badgers;Devastating Donkeys;draw\nAllegoric Alaskans;Courageous Californians;draw',
-    'Team                           | MP |  W |  D |  L |  P\nAllegoric Alaskans             |  3 |  2 |  1 |  0 |  7\nCourageous Californians        |  3 |  2 |  1 |  0 |  7\nBlithering Badgers             |  3 |  0 |  1 |  2 |  1\nDevastating Donkeys            |  3 |  0 |  1 |  2 |  1'
+    'Courageous Californians;Devastating Donkeys;win
+Allegoric Alaskans;Blithering Badgers;win
+Devastating Donkeys;Allegoric Alaskans;loss
+Courageous Californians;Blithering Badgers;win
+Blithering Badgers;Devastating Donkeys;draw
+Allegoric Alaskans;Courageous Californians;draw',
+    'Team                           | MP |  W |  D |  L |  P
+Allegoric Alaskans             |  3 |  2 |  1 |  0 |  7
+Courageous Californians        |  3 |  2 |  1 |  0 |  7
+Blithering Badgers             |  3 |  0 |  1 |  2 |  1
+Devastating Donkeys            |  3 |  0 |  1 |  2 |  1'
   ),
   (
     'f9e20931-8a65-442a-81f6-503c0205b17a',
     'ensure points sorted numerically',
-    'Devastating Donkeys;Blithering Badgers;win\nDevastating Donkeys;Blithering Badgers;win\nDevastating Donkeys;Blithering Badgers;win\nDevastating Donkeys;Blithering Badgers;win\nBlithering Badgers;Devastating Donkeys;win',
-    'Team                           | MP |  W |  D |  L |  P\nDevastating Donkeys            |  5 |  4 |  0 |  1 | 12\nBlithering Badgers             |  5 |  1 |  0 |  4 |  3'
+    'Devastating Donkeys;Blithering Badgers;win
+Devastating Donkeys;Blithering Badgers;win
+Devastating Donkeys;Blithering Badgers;win
+Devastating Donkeys;Blithering Badgers;win
+Blithering Badgers;Devastating Donkeys;win',
+    'Team                           | MP |  W |  D |  L |  P
+Devastating Donkeys            |  5 |  4 |  0 |  1 | 12
+Blithering Badgers             |  5 |  1 |  0 |  4 |  3'
   );
 
 UPDATE tests
--- exercises/practice/twelve-days/create_test_table.sql
+++ exercises/practice/twelve-days/create_test_table.sql
@@ -114,21 +114,36 @@ VALUES
     'recites first three verses of the song',
     1,
     3,
-    'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\nOn the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\nOn the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+    'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
+On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
   ),
   (
     'c095af0d-3137-4653-ad32-bfb899eda24c',
     'recites three verses from the middle of the song',
     4,
     6,
-    'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+    'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
   ),
   (
     '20921bc9-cc52-4627-80b3-198cbbfcf9b7',
     'recites the whole song',
     1,
     12,
-    'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\nOn the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\nOn the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\nOn the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+    'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
+On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
+On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
   );
 
 UPDATE tests
